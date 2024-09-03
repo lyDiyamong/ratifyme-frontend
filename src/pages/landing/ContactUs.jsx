f9145059300a798b88569434795de7e4cabb5809
Maalenn <maalen231219@gmail.com>
2024-09-03 10:25:20 +0700

    feat(11-contactUs): import neccessary mui item to create style for contact us page

---
+++ src/pages/landing/ContactUs.jsx
@@ -0,0 +1,10 @@
+import { Box, Container, Grid, Typography, Button } from "@mui/material";
+import theme from "../../assets/themes/index"
+import ContactUsImg from "../../assets/images/contactUs.svg"
+const ContactUs = () => {
+    return (
+    
+    );
+};
+
+export default ContactUs;
