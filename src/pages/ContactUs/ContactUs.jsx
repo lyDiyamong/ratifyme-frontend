fef6e24e51563a3aae9fd385055291e3aefc2de6
Maalenn <maalen231219@gmail.com>
2024-09-03 09:31:58 +0700

    add(11-contactUs): import material ui and theme configuration

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,7 +1,9 @@
-const ContactUs = () => {
-  return (
-    <div>ContactUs</div>
-  )
-}
+import theme from "../../assets/themes";
+import { Container, Box, Typography, Grid, Button } from "@mui/material";
+import contactusImg from "../../assets/images/contactUs.svg";
 
-export default ContactUs
+const ContactUs = () => {
+    return <div>ContactUs</div>;
+};
+
+export default ContactUs;
