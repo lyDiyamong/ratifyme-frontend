e763420dea2150967e356276347ae09ec7fdfe4e
RothaSAMON <samonrotha@gmail.com>
2024-09-03 09:21:53 +0700

    add(02-navbar): setup the routes for Landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import theme from "./assets/themes";
 import { CssBaseline } from "@mui/material";
+import LandingRouter from "./routers/LandingRouter";
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
+            <LandingRouter />
         </ThemeProvider>
     );
 };
--- src/routers/LandingRouter.jsx
+++ src/routers/LandingRouter.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingLayout from "../layouts/landing";
+import HomePage from "../pages/landing/HomePage";
 
 const LandingRouter = () => {
-  return (
-    <div>LandingRouter</div>
-  )
-}
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<LandingLayout />}>
+                    <Route path="/" element={<HomePage />}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default LandingRouter
+export default LandingRouter;
