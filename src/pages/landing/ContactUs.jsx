ed876419596aa9ff445f29fa26aeb161b90c0381
Maalenn <maalen231219@gmail.com>
2024-09-03 10:31:52 +0700

    add(11-contact-us): add form title and description into contact us form

--- src/pages/landing/ContactUs.jsx
+++ src/pages/landing/ContactUs.jsx
@@ -1,9 +1,36 @@
 import { Box, Container, Grid, Typography, Button } from "@mui/material";
-import theme from "../../assets/themes/index"
-import ContactUsImg from "../../assets/images/contactUs.svg"
+import theme from "../../assets/themes/index";
+import ContactUsImg from "../../assets/images/contactUs.svg";
 const ContactUs = () => {
     return (
-    
+        <Container>
+            <Grid>
+                <Grid
+                    component="form"
+                    item
+                    xs={12}
+                    md={5}
+                    order={{ xs: 2, md: 1 }}
+                >
+                    <Box>
+                        <Typography
+                            component="label"
+                            sx={{
+                                fontWeight: theme.typography.fontWeightLight,
+                                fontSize: theme.typography.h3,
+                            }}
+                        >
+                            Contact Us
+                        </Typography>
+                        <Typography gutterBottom my={2}>
+                            Thank you for your interest in VerifyME by TechA. We
+                            are the experts in digital credentialing, and we are
+                            here to help you:
+                        </Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
