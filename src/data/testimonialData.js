53b7db858f49396e6fa0553c6e6933093f710792
BunthongRorn <bunthongran579@gmail.com>
2024-09-04 17:07:46 +0700

    add(06-WhatWeDo): adjust mobile screen and profile image

--- src/data/testimonialData.js
+++ src/data/testimonialData.js
@@ -1,62 +1,68 @@
-import RatanakImg from '../assets/icons/RatanakImg.svg';
-import LyHour from '../assets/icons/LyHour.svg';
-
+// import image
+import RatanakImg from "../assets/icons/RatanakImg.svg";
+import LyHour from "../assets/icons/LyHour.svg";
+import Rotha from "../assets/icons/Rotha.webp";
+import SreyPov from "../assets/icons/SreyPov.webp";
+import Chounan from "../assets/icons/Chounan.webp";
+import Malen from "../assets/icons/Malen.webp";
+import Yamong from "../assets/icons/Yamong.webp";
+import Manet from "../assets/icons/Manet.webp";
 
 export const testimonials = [
     {
         id: 1,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
         author: "Rathanak",
-        position: "HR of The Lorem Company",
+        position: "Tech A co.,ltd",
         image: RatanakImg,
     },
     {
         id: 2,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
         author: "Ly Hour",
-        position: "HR of The Lorem Company",
+        position: "Tech A co.,ltd",
         image: LyHour,
     },
     {
         id: 3,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
         author: "Rotha",
-        position: "HR of The Lorem Company",
-        image: "path_to_image_1.jpg",
+        position: "Tech A co.,ltd",
+        image: Rotha,
     },
     {
         id: 4,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
         author: "Srey Pov",
-        position: "HR of The Lorem Company",
-        image: "path_to_image_2.jpg",
+        position: "Tech A co.,ltd",
+        image: SreyPov,
     },
     {
         id: 5,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
-        author: "Rathanak",
-        position: "HR of The Lorem Company",
-        image: RatanakImg,
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
+        author: "Chounan",
+        position: "Tech A co.,ltd",
+        image: Chounan,
     },
     {
         id: 6,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
-        author: "Ly Hour",
-        position: "HR of The Lorem Company",
-        image: LyHour,
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
+        author: "Malen",
+        position: "Tech A co.,ltd",
+        image: Malen,
     },
     {
         id: 7,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
-        author: "Rotha",
-        position: "HR of The Lorem Company",
-        image: "path_to_image_1.jpg",
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
+        author: "Yamong",
+        position: "Tech A co.,ltd",
+        image: Yamong,
     },
     {
         id: 8,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
-        author: "Srey Pov",
-        position: "HR of The Lorem Company",
-        image: "path_to_image_2.jpg",
+        text: "Verifyme has made the process of verifying student badges so easy and reliable. It's a game changer for HR departments like ours!",
+        author: "Manet",
+        position: "Tech A co.,ltd",
+        image: Manet,
     },
 ];
--- src/pages/landing/Testimonial.jsx
+++ src/pages/landing/Testimonial.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css'; // Import default styles from the package
+// import react property
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-import { FormatQuote } from '@mui/icons-material';
-import { Card, CardContent, Typography, Avatar, Grid } from '@mui/material';
+// import React MUI components
+import { FormatQuote } from "@mui/icons-material";
+import { Card, CardContent, Typography, Avatar, Grid } from "@mui/material";
 
-import theme from '../../assets/themes';
-
-import { testimonials } from '../../data/testimonialData';
+// import custom components
+import theme from "../../assets/themes";
+import { testimonials } from "../../data/testimonialData";
 
 // Configuration for the carousel responsiveness
 const responsive = {
@@ -16,11 +18,11 @@ const responsive = {
         items: 2,
     },
     desktop: {
-        breakpoint: { max: 1500, min: 1200 },
+        breakpoint: { max: 1500, min: 1000 },
         items: 2,
     },
     tablet: {
-        breakpoint: { max: 1200, min: 768 },
+        breakpoint: { max: 1000, min: 768 },
         items: 1,
     },
     mobile: {
@@ -29,96 +31,147 @@ const responsive = {
     },
 };
 
+//============ start testimonial section ============
+
 const Testimonial = () => {
-  return (
-    <div style={{ padding: '0rem', backgroundColor: '#F9FBFF', maxWidth: '1200px', margin: 'auto', textAlign: 'center' }}>
-      <Typography variant="h4" gutterBottom sx={{ fontWeight: 600, color: theme.palette.text.disabled }}>
-        What Our Clients Say
-      </Typography>
-      <Carousel
-        responsive={responsive}
-        infinite={true} // Makes the carousel loop infinitely
-        autoPlay={true} // Enables autoplay
-        autoPlaySpeed={3000} // Auto slides every 3 seconds
-        keyBoardControl={true} // Enable keyboard navigation
-        swipeable={true} // Allow swiping on touch devices
-        draggable={true} // Enable dragging for mouse users
-        showDots={true} // Show navigation dots
-      >
-        {testimonials.map((testimonial, index) => (
-          <div key={index} style={{ padding: '1rem' }}>
-            <Card
-              sx={{
-                padding: '1.5rem',
-                marginBottom: '1rem',
-                textAlign: 'left',
-                border: '1px solid #ddd',
-                borderRadius: '15px',
-                display: 'flex',
-                alignItems: 'center',
-                height: '100%',
-                minHeight: '230px',
-                [theme.breakpoints.down('sm')]: {
-                  flexDirection: 'column',
-                  textAlign: 'center',
-                  padding: '1rem', // Add more padding for better spacing
-                },
-              }}
+    return (
+        <div
+            style={{
+                padding: "0rem",
+                backgroundColor: "#F9FBFF",
+                maxWidth: "1200px",
+                margin: "auto",
+                textAlign: "center",
+            }}
+        >
+            <Typography
+                variant="h2"
+                gutterBottom
+                sx={{ fontWeight: 600, color: theme.palette.text.disabled }}
             >
-              <Avatar
-                alt={testimonial.author}
-                src={testimonial.image}
-                sx={{
-                  width: '100%', // Adjusted to fit container width on mobile
-                  height: 'auto', // Maintain aspect ratio
-                  maxWidth: '150px', // Maximum size for mobile
-                  marginRight: '1.5rem',
-                  borderRadius: '12px',
-                  [theme.breakpoints.down('sm')]: {
-                    width: '80%', // Smaller size on mobile to fit better
-                    height: 'auto',
-                    marginBottom: '1rem',
-                    marginRight: 0,
-                  },
-                }}
-              />
-              <CardContent>
-                <Grid container spacing={2}>
-                  <Grid item>
-                    <Typography color="primary" variant="h3" component="div">
-                      <FormatQuote sx={{ fontSize: '4rem', rotate: '180deg' }} />
-                    </Typography>
-                  </Grid>
-                  <Grid item xs>
-                    <Typography
-                      variant="body1"
-                      sx={{
-                        fontWeight: 600,
-                        color: theme.palette.text.primary,
-                        marginBottom: '1rem',
-                        [theme.breakpoints.down('sm')]: {
-                          textAlign: 'center',
-                          fontSize: '0.9rem', // Reduced font size for mobile
-                        },
-                      }}
-                    >
-                      {testimonial.text}
-                    </Typography>
-                    <Typography variant="body2" sx={{ fontWeight: 500, color: theme.palette.text.disabled, fontSize: '0.85rem' }}>
-                      {testimonial.author}
-                    </Typography>
-                    <Typography variant="body2" sx={{ fontWeight: 500, color: theme.palette.text.disabled, fontSize: '0.75rem' }}>
-                      {testimonial.position}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </div>
-        ))}
-      </Carousel>
-    </div>
-  );
+                What Our Clients Say
+            </Typography>
+            <Carousel
+                responsive={responsive}
+                infinite={true} // Makes the carousel loop infinitely
+                autoPlay={true} // Enables autoplay
+                autoPlaySpeed={3000} // Auto slides every 3 seconds
+                keyBoardControl={true} // Enable keyboard navigation 
+                swipeable={true} // Allow swiping on touch devices
+                draggable={true} // Enable dragging for mouse users
+                showDots={true} // Show navigation dots
+            >
+                {testimonials.map((testimonial, index) => (
+                    <div key={index} style={{ padding: "1rem" }}>
+                        <Card
+                            sx={{
+                                padding: "1.5rem",
+                                marginBottom: "1rem",
+                                textAlign: "left",
+                                border: "1px solid #ddd",
+                                borderRadius: "15px",
+                                display: "flex",
+                                alignItems: "center",
+                                height: "100%",
+                                minHeight: "230px",
+                                [theme.breakpoints.down("sm")]: {
+                                    flexDirection: "column",
+                                    textAlign: "center",
+                                    padding: "1rem", // Add more padding for better spacing
+                                },
+                            }}
+                        >
+                            <Avatar
+                                alt={testimonial.author}
+                                src={testimonial.image}
+                                sx={{
+                                    width: "100%", // Adjusted to fit container width on mobile
+                                    height: "auto", // Maintain aspect ratio
+                                    maxWidth: "150px", // Maximum size for mobile
+                                    marginRight: "1.5rem",
+                                    borderRadius: "12px",
+                                    [theme.breakpoints.down("sm")]: {
+                                        width: "80%", // Smaller size on mobile to fit better
+                                        height: "auto",
+                                        marginBottom: "1rem",
+                                        marginRight: 0,
+                                    },
+                                }}
+                            />
+                            <CardContent>
+                                <Grid container spacing={2}>
+                                    <Grid item>
+                                        <Typography
+                                            color="primary"
+                                            variant="h3"
+                                            component="div"
+                                            sx={{
+                                                fontSize: "4rem",
+                                                rotate: "180deg",
+                                                display: {
+                                                    xss: "none",
+                                                    sm: "block",
+                                                },
+                                            }}
+                                        >
+                                            <FormatQuote
+                                                sx={{
+                                                    fontSize: "4rem",
+                                                    rotate: "180deg",
+                                                }}
+                                            />
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Typography
+                                            variant="body1"
+                                            sx={{
+                                                fontWeight: 600,
+                                                color: theme.palette.text
+                                                    .primary,
+                                                marginBottom: "1rem",
+                                                [theme.breakpoints.down("sm")]:
+                                                    {
+                                                        textAlign: "center",
+                                                        fontSize: "0.9rem", // Reduced font size for mobile
+                                                    },
+                                            }}
+                                        >
+                                            {testimonial.text}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            sx={{
+                                                fontWeight: 500,
+                                                color: theme.palette.text
+                                                    .disabled,
+                                                fontSize: "0.85rem",
+                                            }}
+                                        >
+                                            {testimonial.author}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            sx={{
+                                                fontWeight: 500,
+                                                color: theme.palette.text
+                                                    .disabled,
+                                                fontSize: "0.75rem",
+                                            }}
+                                        >
+                                            {testimonial.position}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    );
 };
 
 export default Testimonial;
+
+//============ end testimonial section ============
