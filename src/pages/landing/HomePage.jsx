e071645402308534bae07542d78a07baac8d2b90
chounan tep <tch23125@gmail.com>
2024-09-04 12:01:18 +0700

    resolve conflict feat(02-heroSection): add image and herosection to landing

---
+++ src/pages/landing/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+
+const HomePage = () => {
+  return (
+      <div>
+          <HeroSection />
+          HomePage
+      </div>
+  );
+}
+
+export default HomePage
