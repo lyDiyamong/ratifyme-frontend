4cfcf57769d9af75e0d8342648069f921615ddc4
lyDiyamong <diyamong26@gmail.com>
2024-09-04 13:47:27 +0700

    feat(13-authRouter) : - setup router

---
+++ src/routers/AuthRouter.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthLayout from "../layouts/auth";
+import SignupPage from "../pages/auth/SignupPage";
+import LoginPage from "../pages/auth/LoginPage";
+
+const AuthRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AuthLayout />}>
+                    <Route path="/signup" element={<SignupPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AuthRouter;
