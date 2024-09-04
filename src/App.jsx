3038fafec1f7eb655a6f48a0b4e16b42070c7122
lyDiyamong <diyamong26@gmail.com>
2024-09-04 13:47:27 +0700

    feat(13-authRouter) : - setup router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "@emotion/react";
 import theme from "./assets/themes";
 import { CssBaseline } from "@mui/material";
 import LandingRouter from "./routers/LandingRouter";
+import AuthRouter from "./routers/AuthRouter";
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <LandingRouter />
+            <AuthRouter />
         </ThemeProvider>
     );
 };
