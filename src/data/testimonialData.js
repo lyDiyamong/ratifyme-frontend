f6156338c371a2651907c5b807057a60d3ebcd09
BunthongRorn <bunthongran579@gmail.com>
2024-09-04 15:32:26 +0700

    add(06-WhatWeDo): add comme nt

---
+++ src/data/testimonialData.js
@@ -0,0 +1,62 @@
+import RatanakImg from '../assets/icons/RatanakImg.svg';
+import LyHour from '../assets/icons/LyHour.svg';
+
+
+export const testimonials = [
+    {
+        id: 1,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Rathanak",
+        position: "HR of The Lorem Company",
+        image: RatanakImg,
+    },
+    {
+        id: 2,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Ly Hour",
+        position: "HR of The Lorem Company",
+        image: LyHour,
+    },
+    {
+        id: 3,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Rotha",
+        position: "HR of The Lorem Company",
+        image: "path_to_image_1.jpg",
+    },
+    {
+        id: 4,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Srey Pov",
+        position: "HR of The Lorem Company",
+        image: "path_to_image_2.jpg",
+    },
+    {
+        id: 5,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Rathanak",
+        position: "HR of The Lorem Company",
+        image: RatanakImg,
+    },
+    {
+        id: 6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Ly Hour",
+        position: "HR of The Lorem Company",
+        image: LyHour,
+    },
+    {
+        id: 7,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Rotha",
+        position: "HR of The Lorem Company",
+        image: "path_to_image_1.jpg",
+    },
+    {
+        id: 8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, modi, obcaecati explicabo nihil odit laudantium Dohearas voluptatem sunt.",
+        author: "Srey Pov",
+        position: "HR of The Lorem Company",
+        image: "path_to_image_2.jpg",
+    },
+];
--- src/pages/landing/WhatWeDo.jsx
+++ src/pages/landing/WhatWeDo.jsx
@@ -1,10 +1,16 @@
+// import react property 
 import React from 'react';
+
+// import MUI element
 import { Container, Typography, Grid,Box} from '@mui/material';
+
+// import asset file
 import WhatWeDoImg1 from "../../assets/icons/WhatWeDoImg1.svg";
 import WhatWeDoImg2 from "../../assets/icons/WhatWeDoImg2.svg";
 import theme from '../../assets/themes/index';
 
 
+// start What We Do section 
 export default function WhatWeDo () {
   return (
     <Container sx={{ my: 5 , backgroundColor: theme.palette.background, maxWidth:'1200px'}}>
@@ -13,7 +19,7 @@ export default function WhatWeDo () {
         What We Do
       </Typography>
 
-      {/* First Image and Paragraph */}
+      {/* start first image and paragraph */}
       <Grid container spacing={4} alignItems="center">
         <Grid item xs={12} md={6}>
          
@@ -33,13 +39,15 @@ export default function WhatWeDo () {
         </Grid>
       </Grid>
 
-      {/* Second Image and Paragraph */}
+      {/* end first image and paragraph */}
+
+      {/* start second image and paragraph */}
       <Grid container spacing={4} alignItems="center" sx={{ mt: 4 }} >
         <Grid container item xs={12} md={6} sx={{ order: { xs: 2, md: 1 } }} gap={2.5}>
 
-        <Typography sx={{fontSize:theme.typography.h2 , fontWeight: 600, color: theme.palette.text.primary}} >What Our Service Provide ?</Typography>
+        <Typography component="h4" sx={{fontSize:theme.typography.h2 , fontWeight: 600, color: theme.palette.text.primary}} >What Our Service Provide ?</Typography>
 
-          <Typography variant="body1">
+          <Typography variant="body1" >
           Provide the digital credential standard that allows individuals to earn, display, and share verified badges representing their skills and achievements. Each badge contains detailed metadata, such as the issuing organization, criteria for earning the badge, and evidence of the achievement.
           </Typography>
           <Typography variant="body1">
@@ -51,10 +59,14 @@ export default function WhatWeDo () {
 
         </Grid>
       </Grid>
+
+       {/* end second image and paragraph  */}
+
     </Container>
   );
 };
 
+// end What We Do section 
 
 
 
