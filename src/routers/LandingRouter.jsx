c6ce4885a97cd264b9fba54d891f4da3939adcc3
Maalenn <maalen231219@gmail.com>
2024-09-04 17:55:50 +0700

    fix(conflict): rebase

--- src/routers/LandingRouter.jsx
+++ src/routers/LandingRouter.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingLayout from "../layouts/landing";
-import HomePage from "../pages/landing/index";
+import HomePage from "../pages/landing/HomePage";
+import ContactUs from "../pages/landing/ContactUs";
 
 const LandingRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<LandingLayout />}>
-                    <Route path="/" element={<HomePage />}/>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/contactus" element={<ContactUs />} />
                 </Route>
             </Routes>
         </BrowserRouter>
