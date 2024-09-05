adfc3be63947400ff1fce124b26cd5bc489ab070
Lyhour12 <slyhour69@gmail.com>
2024-09-05 09:49:25 +0700

    style:(09-servicePlanCard): - add comments follow convention guide - change style theme follow theme config

--- src/routers/LandingRouter.jsx
+++ src/routers/LandingRouter.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingLayout from "../layouts/landing";
-import HomePage from "../pages/landing/HomePage";
+import HomePage from "../pages/landing";
 import PricePage from "../pages/prices/index";
 
 const LandingRouter = () => {
