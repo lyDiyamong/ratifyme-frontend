1d1e7039de3977f598a9157aaaaf9045f033a7ed
Lyhour12 <slyhour69@gmail.com>
2024-09-05 10:09:18 +0700

    style(09-servicePlanCard) : change maxWidth to use cutom maxWidth in LandingContainer

--- src/pages/prices/index.jsx
+++ src/pages/prices/index.jsx
@@ -3,53 +3,56 @@ import { Box, Paper, Typography } from "@mui/material";
 
 // Custom import
 import PriceCard from "../../components/PriceCard";
+import LandingContainer from "../../components/styles/LandingContainer";
 import { useTheme } from "@mui/material";
 
 const PricePage = () => {
     const theme = useTheme();
     return (
-        <Paper
-            sx={{
-                maxWidth: 1200,
-                mx: "auto",
-                p: 4,
-                display: "flex",
-                flexDirection: "column",
-                gap: 5,
-                boxShadow: theme.shadows.default,
-                borderRadius: theme.customShape.section,
-                mt: 4,
-            }}
-        >
-            <Typography
+        <LandingContainer>
+            <Paper
                 sx={{
-                    fontFamily: theme.typography.fontFamily,
-                    fontSize: theme.typography.h1,
-                    textAlign: "center",
-                    fontWeight: 700,
+                    mx: "auto",
+                    p: 4,
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 5,
+                    boxShadow: theme.shadows.default,
+                    borderRadius: theme.customShape.section,
+                    mt: 4,
                 }}
             >
-                Badge Service Pricing
-            </Typography>
-            <Typography
-                sx={{
-                    fontFamily: theme.typography.fontFamily,
-                    fontSize: theme.typography.body1,
-                }}
-            >
-                Our Digital Badge Service offers a comprehensive solution for
-                recognizing and showcasing achievements in a digital format.
-                Ideal for educational institutions, professional organizations,
-                and corporate training programs, our service allows you to
-                create, manage, and distribute digital badges that are secure,
-                verifiable, and easily shareable across social media platforms.
-                Each badge is customizable, ensuring it reflects your brand
-                identity while celebrating the recipient’s accomplishments.
-            </Typography>
-            <Box sx={{ width: "100%" }}>
-                <PriceCard />
-            </Box>
-        </Paper>
+                <Typography
+                    sx={{
+                        fontFamily: theme.typography.fontFamily,
+                        fontSize: theme.typography.h1,
+                        textAlign: "center",
+                        fontWeight: 700,
+                    }}
+                >
+                    Badge Service Pricing
+                </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: theme.typography.fontFamily,
+                        fontSize: theme.typography.body1,
+                    }}
+                >
+                    Our Digital Badge Service offers a comprehensive solution
+                    for recognizing and showcasing achievements in a digital
+                    format. Ideal for educational institutions, professional
+                    organizations, and corporate training programs, our service
+                    allows you to create, manage, and distribute digital badges
+                    that are secure, verifiable, and easily shareable across
+                    social media platforms. Each badge is customizable, ensuring
+                    it reflects your brand identity while celebrating the
+                    recipient’s accomplishments.
+                </Typography>
+                <Box sx={{ width: "100%" }}>
+                    <PriceCard />
+                </Box>
+            </Paper>
+        </LandingContainer>
     );
 };
 
