977cbf7a7d2928138c54fdd4426a948acb12ac34
Prak Sreypov <sreypovprak88@gmail.com>
2024-09-05 11:58:05 +0700

    refactor(12-dashboard-layout): refactor router

--- src/routers/DashboardRouter.jsx
+++ src/routers/DashboardRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import DashboardLayout from "../layouts/dasboard";
 import Dashboard from "../pages/dasboard";
 import BadgeManagement from "../pages/badgeMangements";
@@ -10,24 +10,24 @@ import BillingInvoiceManagement from "../pages/billingInvoiceManagement";
 import Report from "../pages/report";
 import AccountManagement from "../pages/setting/accountManagement";
 import PrivacySecurityManagement from "../pages/setting/privacyManagement";
+import BackpackManagement from "../pages/backpackManagement";
 
 const DashbaordRouter = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<DashboardLayout />}>
-                    <Route path="/dashboard" element={<Dashboard />}/>
-                    <Route path="/management/badges" element={<BadgeManagement />}/>
-                    <Route path="/management/issuers" element={<IssuerManagement />}/>
-                    <Route path="/management/eaners" element={<EarnerManagement />}/>
-                    <Route path="/sales/servicePlan" element={<ServicePlanManagement />}/>
-                    <Route path="/sales/billing&invoice" element={<BillingInvoiceManagement />}/>
-                    <Route path="/reports" element={<Report />}/>
-                    <Route path="/setting/account" element={<AccountManagement />}/>
-                    <Route path="/setting/privacy&security" element={<PrivacySecurityManagement />}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route element={<DashboardLayout />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/management/badges" element={<BadgeManagement />} />
+                <Route path="/management/issuers" element={<IssuerManagement />} />
+                <Route path="/management/eaners" element={<EarnerManagement />} />
+                <Route path="/sales/servicePlan" element={<ServicePlanManagement />} />
+                <Route path="/sales/billing&invoice" element={<BillingInvoiceManagement />} />
+                <Route path="/reports" element={<Report />} />
+                <Route path="/mybackpacks" element={<BackpackManagement />} />
+                <Route path="/setting/account" element={<AccountManagement />} />
+                <Route path="/setting/privacy&security" element={<PrivacySecurityManagement />} />
+            </Route>
+        </Routes>
     );
 };
 
