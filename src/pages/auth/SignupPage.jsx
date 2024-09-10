45898be0a11b0f82ac96205a77e17be4111ab1e3
Sreypovprak <sreypovprak88@gmail.com>
2024-09-10 15:12:34 +0700

    add(28-signupIntegrate): integrate signup api with react Signup Page

--- src/pages/auth/SignupPage.jsx
+++ src/pages/auth/SignupPage.jsx
@@ -1,6 +1,5 @@
 // React library import
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
 
 // MUI import
 import { Box, Grid, Typography, Button } from "@mui/material";
@@ -12,43 +11,27 @@ import LandingContainer from "../../components/styles/LandingContainer";
 import SignupImgSvg from "../../assets/images/Signup-illu.svg";
 import { useSignupMutation } from "../../store/api/auth/authApi";
 
-function SignupPage() {
-    // form controller
+const SignupPage = () => {
     const { handleSubmit, control } = useForm();
+    const [signUp, { isLoading, isError, error }] = useSignupMutation();
 
-    // Use the RTK Query mutation hook
-    const [signUp, { isLoading, isSuccess, isError, error }] = useSignupMutation();
-
-    // handle submission
     const onSubmit = async (data) => {
         try {
-            // Call the signUp mutation
-            const result = await signUp(data).unwrap(); // Unwrap result to handle errors
-
-            // Handle success response
+            const result = await signUp(data).unwrap();
             console.log("Signup successful:", result);
         } catch (err) {
-            // Handle error response
             console.error("Error during signup:", err);
         }
     };
 
     return (
-        // ============ Start container ============
         <LandingContainer sx={{ my: 6 }}>
-            {/* Start grid container */}
             <Grid container spacing={4}>
-                {/* Start signup section */}
                 <Grid item xs={12} md={4} order={{ xs: 2, md: 1 }}>
-                    {/* Top text container */}
                     <Box mb={5}>
                         <Typography
                             component="h4"
-                            sx={{
-                                fontSize: theme.typography.h2,
-                                fontWeight: theme.fontWeight.bold,
-                                lineHeight: 2,
-                            }}
+                            sx={{ fontSize: theme.typography.h2, fontWeight: theme.fontWeight.bold, lineHeight: 2 }}
                         >
                             Sign up
                         </Typography>
@@ -57,37 +40,33 @@ function SignupPage() {
                             with us.
                         </Typography>
                     </Box>
-                    {/* Start form Container */}
                     <Box
                         component="form"
                         onSubmit={handleSubmit(onSubmit)}
                         display="flex"
                         flexDirection="column"
                         gap={3}
-                        noValidate // Reset to default
+                        noValidate
                     >
-                        {/* Username */}
-                        <FormInput
-                            name="username"
-                            label="Username"
-                            control={control}
-                            type="text"
-                            required={true}
-                            // Additional props if needed
-                        />
-                        {/* Email */}
+                        <FormInput name="username" label="Username" control={control} type="text" required={true} />
+                        {/* <FormInput name="firstname" label="Firstname" control={control} type="text" required={true} /> */}
+                        {/* <FormInput name="lastname" label="Lastname" control={control} type="text" required={true} /> */}
                         <FormInput label="Email" name="email" control={control} required={true} type="email" />
-                        {/* Password */}
+                        <FormInput
+                            label="Phone Number"
+                            name="phoneNumber"
+                            control={control}
+                            required={true}
+                            type="text"
+                        />
                         <FormInput label="Password" name="password" control={control} type="password" required={true} />
-                        {/* Confirm password */}
                         <FormInput
                             label="Confirm Password"
-                            name="cfPassword"
+                            name="passwordConfirm"
                             control={control}
                             type="password"
                             required={true}
                         />
-                        {/* Submit button */}
                         <Button
                             fullWidth
                             type="submit"
@@ -97,70 +76,50 @@ function SignupPage() {
                                 borderRadius: theme.customShape.btn,
                                 fontWeight: theme.fontWeight.bold,
                             }}
+                            disabled={isLoading}
                         >
                             {isLoading ? "Signing Up..." : "Sign Up"}
                         </Button>
                         {isError && <Typography color="error">{error?.data?.message || "Signup failed"}</Typography>}
-                        {/* Text description */}
                         <Typography textAlign="center" color={theme.palette.text.secondary}>
                             By signing up to create an account I accept{" "}
                             <Typography
                                 component="span"
-                                sx={{
-                                    fontWeight: theme.fontWeight.semiBold,
-                                    color: theme.palette.text.primary,
-                                }}
+                                sx={{ fontWeight: theme.fontWeight.semiBold, color: theme.palette.text.primary }}
                             >
                                 Term of Use{" "}
                             </Typography>
                             and{" "}
                             <Typography
                                 component="span"
-                                sx={{
-                                    fontWeight: theme.fontWeight.semiBold,
-                                    color: theme.palette.text.primary,
-                                }}
+                                sx={{ fontWeight: theme.fontWeight.semiBold, color: theme.palette.text.primary }}
                             >
                                 Privacy Policy.
                             </Typography>
                         </Typography>
                         <Typography textAlign="center" color={theme.palette.text.secondary}>
                             Already have an account?{" "}
-                            <Link to="/login">
+                            {/* <Link to="/login">
                                 <Typography
                                     component="span"
-                                    sx={{
-                                        color: theme.palette.text.primary,
-                                        fontWeight: theme.fontWeight.semiBold,
-                                    }}
+                                    sx={{ color: theme.palette.text.primary, fontWeight: theme.fontWeight.semiBold }}
                                 >
                                     Log in
                                 </Typography>{" "}
-                            </Link>
+                            </Link> */}
                         </Typography>
                         <Typography color={theme.palette.text.secondary} fontSize={theme.typography.body2}>
                             Let’s get started. Are you ready to be a part of our digital badge? Then boldly move forward
                             with us.
                         </Typography>
                     </Box>
-                    {/* End form container */}
                 </Grid>
-                {/* End signup section */}
-
-                {/* Image container */}
                 <Grid item xs={12} md={8} order={{ xs: 1, md: 2 }}>
-                    <Box
-                        component="img"
-                        sx={{
-                            width: "100%",
-                        }}
-                        alt="illustration"
-                        src={SignupImgSvg}
-                    />
+                    <Box component="img" sx={{ width: "100%" }} alt="illustration" src={SignupImgSvg} />
                 </Grid>
             </Grid>
         </LandingContainer>
     );
-}
+};
 
 export default SignupPage;
