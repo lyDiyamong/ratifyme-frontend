c9bbeed0cc6fa88e779019bf80f1b56821d60038
RatanakANB <rattanakphok9@gmail.com>
2024-09-11 09:37:49 +0700

    add(20-DetailBadge): Title and Detail Are now dynamic as render from DataBadgedetailed.jsx

--- src/pages/earnerManagements/BadgeDetail.jsx
+++ src/pages/earnerManagements/BadgeDetail.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 
-import {Box} from "@mui/material"
+import {Box, Typography, Stack } from "@mui/material"
 
 import theme from "../../assets/themes";
 import PageSection from "./PageSection";
 import DashboardContainer from "../../components/styles/DashboardContainer";
+import { TitleData, DetailData } from "./DataBadgeDetialed";
+
+
 
 const BadgeDetail = () => {
     return (
         <DashboardContainer>
             <PageSection>
-                <Box>
-                    test
-                </Box>
+                <Stack flexDirection={{ xs: "column", md: "row" }}>
+                    <Box sx={{m:2}}>
+                        {TitleData.items.map((item) => (
+                            <Typography
+                                key={item.id}
+                                variant="body1"
+                                sx={{textAlign: "right", }}>
+                                {item.id}
+                            </Typography>
+                        ))}
+                    </Box>
+                    <Box sx={{m:2}}>
+                        {DetailData.items.map((item) => (
+                            <Typography
+                                key={item.id}
+                                variant="body1"
+                                sx={{textAlign: "right", }}>
+                                {item.id}
+                            </Typography>
+                        ))}
+                    </Box>
+                </Stack>
             </PageSection>
         </DashboardContainer>
     )
---
+++ src/pages/earnerManagements/DataBadgeDetialed.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TitleData = {
+    items: [
+        { id: 1, title: 'Item 1' },
+        { id: 2, title: 'Item 2' },
+    // ... more items
+    ],
+};
+
+const DetailData = {
+    items: [
+        { id: 1, title: 'Item 1' },
+        { id: 2, title: 'Item 2' },
+      // ... more items
+    ],
+};
+
+export {TitleData, DetailData}
