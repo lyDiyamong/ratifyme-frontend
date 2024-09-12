0d06da083cd42a99a8b8bc1db9fe895b9acad9aa
Sreypovprak <sreypovprak88@gmail.com>
2024-09-12 08:49:55 +0700

    fix(28-signup-form): resolve conflict

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
+import { authApi } from "./api/auth/authApi";
 
 const store = configureStore({
     reducer: {
-        
+        [authApi.reducerPath]: authApi.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
 });
-setupListeners(store.dispatch);
 
 export default store;
