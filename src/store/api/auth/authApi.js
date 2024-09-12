916fe06cfe5f399ea8f1394590f055cad7da99b1
Sreypovprak <sreypovprak88@gmail.com>
2024-09-12 12:58:12 +0700

    refactor(28-signup): config VITE environment

--- src/store/api/auth/authApi.js
+++ src/store/api/auth/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const authApi = createApi({
     reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.SERVER_BASE_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_SERVER_BASE_URL }),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (data) => ({
