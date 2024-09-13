dc252fc2e6fbf8b8de855d5f5abfe5e3a1237086
lyDiyamong <diyamong26@gmail.com>
2024-09-13 11:04:14 +0700

    test(testCheckout): test integrating with api

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -3,6 +3,7 @@ import { Box, Paper, Typography, Button } from "@mui/material";
 
 // Custom import
 import theme from "../assets/themes";
+import CheckoutButton from "./CheckoutButton";
 
 const PriceCard = ({ item }) => {
     return (
@@ -16,7 +17,7 @@ const PriceCard = ({ item }) => {
                 gap: 5,
                 borderRadius: theme.customShape.section,
                 px: 2,
-                boxShadow: theme.customShadows.default
+                boxShadow: theme.customShadows.default,
             }}
         >
             <Box>
@@ -50,18 +51,7 @@ const PriceCard = ({ item }) => {
             >
                 $ {item.price}
             </Typography>
-            <Button
-                sx={{
-                    bgcolor: theme.palette.primary.main,
-                    color: theme.palette.customColors.white,
-                    width: "100%",
-                    fontSize: theme.typography.body1,
-                    fontWeight: theme.fontWeight.semiBold,
-                    borderRadius: theme.customShape.btn,
-                }}
-            >
-                Subscribe
-            </Button>
+            <CheckoutButton id={item.productId} />
         </Paper>
     );
 };
