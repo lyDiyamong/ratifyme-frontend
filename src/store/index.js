330088dd0e56e3a9531152626566a043d65bef82
lyDiyamong <diyamong26@gmail.com>
2024-09-13 11:39:17 +0700

    test(testCheckout): create api subscription in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./api/auth/authApi";
 import { setupListeners } from '@reduxjs/toolkit/query';
+import { subscriptionApi } from "./api/subscription/subscriptionApi";
 
 const store = configureStore({
     reducer: {
         [authApi.reducerPath]: authApi.reducer,
+        [subscriptionApi.reducerPath] : subscriptionApi.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware, subscriptionApi.middleware),
 });
 setupListeners(store.dispatch);
 
