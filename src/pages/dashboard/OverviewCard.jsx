057fd5a92ae3cd7d8835fc9d8ac9afb5bccb11a4
chounan tep <tch23125@gmail.com>
2024-09-13 13:50:48 +0700

    add(24-UserProfile): add icon image

--- src/pages/dashboard/OverviewCard.jsx
+++ src/pages/dashboard/OverviewCard.jsx
@@ -43,7 +43,7 @@ const CardsList = () => {
                         backgroundColor: theme.palette.customColors.white,
                     }}
                 >
-                    {" "}
+                    
                     {/* Image graph */}
                     <CardMedia
                         component="img"
