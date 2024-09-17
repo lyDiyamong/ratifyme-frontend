3df11f31083cec1b2e058a021be1c8e13435dec2
Lyhour12 <slyhour69@gmail.com>
2024-09-17 18:51:33 +0700

    add(31-integrateBadgeList): create multi selection component

---
+++ src/components/MultiSelectionForm.jsx
@@ -0,0 +1,73 @@
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import FormHelperText from "@mui/material/FormHelperText";
+import { useController } from "react-hook-form";
+import theme from "../assets/themes";
+
+/**
+ * A reusable SelectForm component for use with react-hook-form.
+ *
+ * @param {string} name - The name of the form field. It should match the key in the form data.
+ * @param {Object} control - The react-hook-form control object used to manage form state.
+ * @param {Array} options - An array of option objects for the select input. Each object should have `value` and `label` properties.
+ * @param {string} label - The label for the select input field.
+ * @param {boolean} required - A boolean indicating if the field is required.
+ */
+const SelectForm = ({ name, control, options, label, required }) => {
+    const validationRules = { required: required ? `${label} is required` : false };
+    const {
+        field,
+        fieldState: { error },
+    } = useController({ name, control, rules: validationRules });
+
+    // This handles the change event and updates the value in react-hook-form
+    const handleChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+
+        // Update react-hook-form with the new value
+        // The value will be an array of selected values
+        field.onChange(value);
+    };
+
+    return (
+        <FormControl fullWidth error={!!error}>
+            <InputLabel id={`${name}-label`}>{label}</InputLabel>
+            <Select
+                multiple // Enable multiple selection
+                required={required}
+                sx={{
+                    borderRadius: theme.customShape.input,
+                }}
+                labelId={`${name}-label`}
+                id={`${name}-select`}
+                value={field.value || []} // Default to an empty array for multi-select
+                label={label}
+                onChange={handleChange}
+                onBlur={field.onBlur}
+                inputProps={{ "aria-required": required }}
+                MenuProps={{
+                    PaperProps: {
+                        sx: {
+                            borderRadius: theme.customShape.input,
+                        },
+                    },
+                }}
+            >
+                {/* Render options for the select input */}
+                {options.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </Select>
+            {/* Display validation error message if any */}
+            {error && <FormHelperText>{error.message}</FormHelperText>}
+        </FormControl>
+    );
+};
+
+export default SelectForm;
