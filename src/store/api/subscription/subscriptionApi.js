8ebf719fa13f835af8973c77effe7600bc7b80ec
lyDiyamong <diyamong26@gmail.com>
2024-09-18 18:32:49 +0700

    feat(33-billinInvoice): add get subscription api in store

--- src/store/api/subscription/subscriptionApi.js
+++ src/store/api/subscription/subscriptionApi.js
@@ -6,10 +6,16 @@ export const subscriptionApi = createApi({
         baseUrl: import.meta.env.VITE_SERVER_BASE_URL,
     }),
     endpoints: (builder) => ({
+
+        // ServicePlans api
         getServicePlan: builder.query({
             query: () => "/subscriptions/servicePlans",
         }),
+        // Subscriptions Api
+        getSubscritption : builder.query({
+            query : () => "/subscriptions"
+        })
     }),
 });
 
-export const { useGetServicePlanQuery } = subscriptionApi;
+export const { useGetServicePlanQuery, useGetSubscritptionQuery } = subscriptionApi;
