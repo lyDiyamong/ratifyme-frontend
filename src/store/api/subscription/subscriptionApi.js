4d2e851a208cbfd99e1b59b56b5dfed5423ffd04
lyDiyamong <diyamong26@gmail.com>
2024-09-18 14:43:30 +0700

    add(testClient) : add comment to checkout button description

--- src/store/api/subscription/subscriptionApi.js
+++ src/store/api/subscription/subscriptionApi.js
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const subscriptionApi = createApi({
-  reducerPath: "subscriptions",
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_API,
-  }),
-  endpoints: (builder) => ({
-    getServicePlan: builder.query({
-      query: () => "/subscriptions/servicePlan",
+    reducerPath: "subscriptions",
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_SERVER_BASE_URL,
     }),
-  }),
+    endpoints: (builder) => ({
+        getServicePlan: builder.query({
+            query: () => "/subscriptions/servicePlans",
+        }),
+    }),
 });
 
 export const { useGetServicePlanQuery } = subscriptionApi;
