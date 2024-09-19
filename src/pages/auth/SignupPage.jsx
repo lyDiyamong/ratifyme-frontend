21e13b476dfd330669897724fc2d7a56d870b858
Sreypovprak <sreypovprak88@gmail.com>
2024-09-19 18:14:34 +0700

    add(30-signupIntegration): add institution steppers for signup form

--- src/pages/auth/SignupPage.jsx
+++ src/pages/auth/SignupPage.jsx
@@ -12,19 +12,58 @@ import DateSelectionForm from "../../components/DateSelectionForm";
 import SelectForm from "../../components/SelectionForm";
 import FormInput from "../../components/FormInput";
 
-const steps = ["General Information", "Address Information", "Account Setup"];
-
-// Data static of the role select
 const roleIdData = {
     institution: 2,
     issuer: 3,
     earner: 4,
 };
 
-const optionSelect = [
+const genderOptions = [
     { value: 1, label: "Male" },
     { value: 2, label: "Female" },
 ];
+
+// Address fields extracted for reuse
+const AddressFields = ({ control }) => (
+    <>
+        <Grid item xs={12} sm={6}>
+            <Controller
+                name="country"
+                control={control}
+                render={({ field: { onChange, onBlur, value, ref } }) => (
+                    <Select
+                        options={countryList().getData()}
+                        onChange={(selectedOption) => onChange(selectedOption.value)}
+                        onBlur={onBlur}
+                        value={countryList()
+                            .getData()
+                            .find((option) => option.value === value)}
+                        inputRef={ref}
+                        placeholder="Select Country"
+                        styles={{
+                            container: (base) => ({ ...base, width: "100%" }),
+                            control: (base) => ({
+                                ...base,
+                                height: "56px",
+                                borderRadius: theme.customShape.input,
+                                background: "none",
+                            }),
+                        }}
+                    />
+                )}
+            />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+            <FormInput name="city" label="City / State" control={control} type="text" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+            <FormInput name="street" label="Street Address" control={control} type="text" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+            <FormInput name="postalCode" label="Postal Code" control={control} type="text" />
+        </Grid>
+    </>
+);
 
 const SignupPage = () => {
     const { search } = useLocation();
@@ -32,6 +71,11 @@ const SignupPage = () => {
     const [role, setRole] = useState("");
     const [signUp, { isLoading, isError, error }] = useSignUpMutation();
     const [activeStep, setActiveStep] = useState(0);
+
+    const steps = {
+        institution: ["General Information", "Address Information", "Account Setup", "Institution Information"],
+        default: ["General Information", "Address Information", "Account Setup"],
+    };
 
     useEffect(() => {
         const queryRole = new URLSearchParams(search).get("as") || "";
@@ -52,77 +96,55 @@ const SignupPage = () => {
             city: "",
             street: "",
             postalCode: "",
+            companyName: "",
+            companyRegistration: "",
         },
     });
 
-    const { handleSubmit, control, getValues } = methods;
+    const { handleSubmit, control } = methods;
 
     const onSubmit = async (data) => {
         const roleId = roleIdData[role] || 0;
         const reqData = {
-            userData: {
-                ...data,
-                roleId,
-            },
-            addressData: {
-                street: data.street,
-                city: data.city,
-                postalCode: data.postalCode,
-                country: data.country,
-            },
+            userData: { ...data, roleId },
+            addressData: { street: data.street, city: data.city, postalCode: data.postalCode, country: data.country },
         };
+
+        if (roleId === roleIdData.institution) {
+            reqData.institutionData = {
+                institutionName: data.institutionName,
+                institutionEmail: data.institutionEmail,
+                institutionPhoneNumber: data.institutionPhoneNumber,
+                institutionWebsiteUrl: data.institutionWebsiteUrl,
+            };
+        }
+
         try {
-            const result = await signUp(reqData).unwrap();
-            navigate("/dashboard");
+            await signUp(reqData).unwrap();
+            role === "institution" ? navigate("/price") : navigate("/dashboard");
         } catch (err) {
             console.error("Error during signup:", err);
         }
     };
 
-    const options = countryList().getData();
+    const handleNext = () => setActiveStep((prev) => prev + 1);
+    const handleBack = () => setActiveStep((prev) => prev - 1);
 
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleStepContent = (step) => {
+    const renderStepContent = (step) => {
         switch (step) {
             case 0:
                 return (
                     <Box>
-                        {/* General Information */}
                         <Typography variant="h4">General Information</Typography>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6}>
-                                <FormInput
-                                    name="firstName"
-                                    label="First Name"
-                                    control={control}
-                                    type="text"
-                                    required={true}
-                                />
+                                <FormInput name="firstName" label="First Name" control={control} required />
                             </Grid>
                             <Grid item xs={12} sm={6}>
-                                <FormInput
-                                    name="lastName"
-                                    label="Last Name"
-                                    control={control}
-                                    type="text"
-                                    required={true}
-                                />
+                                <FormInput name="lastName" label="Last Name" control={control} required />
                             </Grid>
                             <Grid item xs={12} sm={6}>
-                                <SelectForm
-                                    control={control}
-                                    name="genderId"
-                                    label="Gender"
-                                    options={optionSelect}
-                                    required={false}
-                                />
+                                <SelectForm name="genderId" label="Gender" options={genderOptions} control={control} />
                             </Grid>
                             <Grid item xs={12} sm={6}>
                                 <DateSelectionForm control={control} name="dateOfBirth" label="Date of Birth" />
@@ -133,85 +155,72 @@ const SignupPage = () => {
             case 1:
                 return (
                     <Box>
-                        {/* Address Information */}
                         <Typography variant="h4">Address Information</Typography>
                         <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <Controller
-                                    name="country"
-                                    control={control}
-                                    render={({ field: { onChange, onBlur, value, ref } }) => (
-                                        <Select
-                                            options={options}
-                                            onChange={(selectedOption) => onChange(selectedOption.value)}
-                                            onBlur={onBlur}
-                                            value={options.find((option) => option.value === value)}
-                                            inputRef={ref}
-                                            placeholder="Select Country"
-                                            styles={{
-                                                container: (base) => ({
-                                                    ...base,
-                                                    width: "100%",
-                                                }),
-                                                control: (base) => ({
-                                                    ...base,
-                                                    height: "56px",
-                                                    borderRadius: theme.customShape.input,
-                                                    background: "none",
-                                                }),
-                                            }}
-                                        />
-                                    )}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <FormInput name="city" label="City / State" control={control} type="text" />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <FormInput name="street" label="Street Address" control={control} type="text" />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <FormInput name="postalCode" label="Postal Code" control={control} type="text" />
-                            </Grid>
+                            <AddressFields control={control} />
                         </Grid>
                     </Box>
                 );
             case 2:
                 return (
                     <Box>
-                        {/* Account Setup */}
                         <Typography variant="h4">Account Setup</Typography>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6}>
+                                <FormInput name="username" label="Username" control={control} required />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormInput name="phoneNumber" label="Phone Number" control={control} required />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormInput name="email" label="Email" control={control} required />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
                                 <FormInput
-                                    name="username"
-                                    label="Username"
+                                    name="password"
+                                    label="Password"
                                     control={control}
-                                    type="text"
-                                    required={true}
+                                    required
+                                    type="password"
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6}>
                                 <FormInput
-                                    label="Phone Number"
-                                    name="phoneNumber"
+                                    name="passwordConfirm"
+                                    label="Confirm Password"
                                     control={control}
-                                    required={true}
-                                    type="text"
+                                    required
+                                    type="password"
                                 />
                             </Grid>
                         </Grid>
-                        <FormInput name="email" label="Email" control={control} type="email" required={true} />
-                        <FormInput label="Password" name="password" control={control} type="password" required={true} />
-                        <FormInput
-                            label="Confirm Password"
-                            name="passwordConfirm"
-                            control={control}
-                            type="password"
-                            required={true}
-                        />
                     </Box>
                 );
+            case 3:
+                return role === "institution" ? (
+                    <Box>
+                        <Typography variant="h4">Institution Information</Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <FormInput name="institutionName" label="Name" control={control} required />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormInput name="institutionEmail" label="Email Address" control={control} required />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormInput
+                                    name="institutionPhoneNumber"
+                                    label="Phone Number"
+                                    control={control}
+                                    required
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormInput name="institutionWebsiteUrl" label="Website" control={control} required />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                ) : null;
             default:
                 return null;
         }
@@ -220,40 +229,59 @@ const SignupPage = () => {
     return (
         <LandingContainer sx={{ my: 6 }}>
             <Grid container spacing={4}>
-                <Grid item xs={12} md={6}>
-                    {/* Signup Form with Stepper */}
+                <Grid item xs={12} md={role === "institution" ? 12 : 6}>
                     <Typography variant="h4" mb={4}>
                         Sign up as {role}
                     </Typography>
                     <Stepper activeStep={activeStep}>
-                        {steps.map((label, index) => (
-                            <Step key={index}>
-                                <StepLabel>{label}</StepLabel>
-                            </Step>
-                        ))}
+                        {steps[role]
+                            ? steps[role].map((label, index) => (
+                                    <Step key={index}>
+                                        <StepLabel>{label}</StepLabel>
+                                    </Step>
+                                ))
+                            : steps.default.map((label, index) => (
+                                    <Step key={index}>
+                                        <StepLabel>{label}</StepLabel>
+                                    </Step>
+                                ))}
                     </Stepper>
                     <FormProvider {...methods}>
                         <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 4 }}>
-                            {handleStepContent(activeStep)}
+                            {renderStepContent(activeStep)}
                             <Stack direction="row" justifyContent="space-between" sx={{ mt: 4 }}>
                                 <Button disabled={activeStep === 0} onClick={handleBack} variant="outlined">
                                     Back
                                 </Button>
                                 <Button
                                     variant="contained"
-                                    onClick={activeStep === steps.length - 1 ? handleSubmit(onSubmit) : handleNext}
+                                    onClick={
+                                        activeStep === (steps[role]?.length - 1 || 2)
+                                            ? handleSubmit(onSubmit)
+                                            : handleNext
+                                    }
                                     disabled={isLoading}
                                 >
-                                    {isLoading ? "Processing..." : activeStep === steps.length - 1 ? "Finish" : "Next"}
+                                    {isLoading
+                                        ? "Processing..."
+                                        : activeStep === (steps[role]?.length - 1 || 2)
+                                        ? "Finish"
+                                        : "Next"}
                                 </Button>
                             </Stack>
                         </Box>
                     </FormProvider>
-                    {isError && <Typography color="error">{error?.data?.message || "Signup failed"}</Typography>}
+                    {isError && (
+                        <Typography color="error" sx={{ mt: 2 }}>
+                            Error: {error.message}
+                        </Typography>
+                    )}
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <Box component="img" sx={{ width: "100%" }} alt="illustration" src={SignupImgSvg} />
-                </Grid>
+                {role !== "institution" && (
+                    <Grid item xs={12} md={6}>
+                        <Box component="img" src={SignupImgSvg} alt="Signup Illustration" sx={{ width: "100%" }} />
+                    </Grid>
+                )}
             </Grid>
         </LandingContainer>
     );
