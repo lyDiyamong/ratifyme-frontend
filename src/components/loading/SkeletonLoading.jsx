ef5479af0b332e9d4a1d1d0617e6d947b288a552
lyDiyamong <diyamong26@gmail.com>
2024-09-23 14:41:22 +0700

    add(36-payment): add skeleton component

---
+++ src/components/loading/SkeletonLoading.jsx
@@ -0,0 +1,21 @@
+import { Box, Skeleton } from "@mui/material";
+
+/**
+ *
+ *
+ * @param {Number}  num : number of skeleton
+ * @return {JSX.Element} The rendered SkeletonLoading component.
+ */
+const SkeletonLoading = ({ num = 3 }) => {
+    return (
+        <>
+            {Array(num)
+                .fill(1)
+                .map((val, index) => (
+                    <Skeleton key={index} height={50} />
+                ))}
+        </>
+    );
+};
+
+export default SkeletonLoading;
