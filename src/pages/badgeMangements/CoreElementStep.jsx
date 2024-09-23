6baf4814ab1752a75a048692d9d8d7c0a52a56af
Lyhour12 <slyhour69@gmail.com>
2024-09-23 19:17:35 +0700

    change(35-badgeDetailIntegrate): change form of issuer name to read only mode and adjust multi form to be short

--- src/pages/badgeMangements/CoreElementStep.jsx
+++ src/pages/badgeMangements/CoreElementStep.jsx
@@ -11,12 +11,23 @@ import { useFetchAchievementTypeQuery } from "../../store/api/achievements/achie
 const CoreElementStep = ({ control }) => {
     // Data static of the earning criteria select
     const { data: achievementType } = useFetchAchievementTypeQuery();
-    
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <Stack gap={2}>
                 {/* Issuer */}
-                <FormInput name="issuer" label="Issuer*" control={control} type="text" required={false} />
+                <FormInput
+                    name="issuer"
+                    label="Issuer*"
+                    control={control}
+                    type="text"
+                    required={false}
+                    slotProps={{
+                        input: {
+                            readOnly: true,
+                        },
+                    }}
+                />
 
                 {/* Criteria */}
                 <FormInput name="narrative" label="Criteria" control={control} type="text" required={false} />
