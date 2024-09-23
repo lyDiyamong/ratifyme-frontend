94e0b3a70b4198f26c96973cc67e5b92b7541b26
Khun Malen <malen8622@gmail.com>
2024-09-23 15:11:08 +0700

    refactor(27-dashbaord): add new no record data to report management page

--- src/pages/report/TableReport.jsx
+++ src/pages/report/TableReport.jsx
@@ -1,24 +1,30 @@
+// React Library
+import { useState } from "react";
+
 // MUI Import
 import { Box, Typography, CircularProgress } from "@mui/material";
 
 // Custom Import
-// import SearchBar from "../../components/SearchBarCustom";
+import SearchBarCustom from "../../components/SearchBarCustom";
 import TableCustom from "../../components/TableList";
 import FormatYear from "../../utils/formatDate";
-
+import NoRecordData from "../../components/NoRecordData";
 // Fetching Data Import
 import { useFetchInstitutionStatsQuery } from "../../store/api/reports/institutionStatApis";
 
 // ============ Start Table Report ============
 const TableReport = () => {
     const { data: response, isLoading, isError } = useFetchInstitutionStatsQuery();
-    const badgeData = response?.data;
+    const reportData = response?.data;
+
+    //Search query state for filtering report data
+    const [searchQuery, setSearchQuery] = useState("");
 
     // Institution Columns
-    const badgeColumns = [
+    const reportColumns = [
         {
             name: "Organization",
-            selector: (row) => row.name,
+            selector: (row) => row.institutionName,
             sortable: true,
         },
         {
@@ -28,45 +34,52 @@ const TableReport = () => {
         },
         {
             name: "Total Badge",
-            selector: (row) => 
-                row.Issuers.reduce((totalBadges, issuer) => 
-                    totalBadges + (issuer.BadgeClasses?.length || 0), 0),
+            selector: (row) =>
+                row.Issuers.reduce((totalBadges, issuer) => totalBadges + (issuer.BadgeClasses?.length || 0), 0),
             sortable: true,
         },
         {
             name: "Total Earner",
-            selector: (row) => 
-                row.Issuers.reduce((totalEarners, issuer) => 
-                    totalEarners + (issuer.Earners?.length || 0), 0),
+            selector: (row) =>
+                row.Issuers.reduce((totalEarners, issuer) => totalEarners + (issuer.Earners?.length || 0), 0),
             sortable: true,
         },
         {
             name: "Expire Date",
             selector: (row) => {
-                const latestBadge = row.Issuers
-                    .flatMap((issuer) => issuer.BadgeClasses)
-                    .reduce((latest, badge) => {
-                        if (!latest || new Date(badge.expiredDate) > new Date(latest.expiredDate)) {
-                            return badge;
-                        }
-                        return latest;
-                    }, null);
-                
+                const latestBadge = row.Issuers.flatMap((issuer) => issuer.BadgeClasses).reduce((latest, badge) => {
+                    if (!latest || new Date(badge.expiredDate) > new Date(latest.expiredDate)) {
+                        return badge;
+                    }
+                    return latest;
+                }, null);
+
                 return latestBadge ? FormatYear({ dateString: latestBadge.expiredDate }) : "N/A";
             },
             sortable: true,
         },
     ];
 
+    //FilterReport Data = reportData?.
+
+    const filterReportData = reportData?.filter((report) =>
+        report.institutionName.toLowerCase().includes(searchQuery),
+    );
+
     return (
         <Box>
+            {/* Search bar */}
+            <SearchBarCustom onSearch={setSearchQuery} />
             {/* Table Data Rendering */}
             {isLoading ? (
                 <CircularProgress />
             ) : isError ? (
                 <Typography color="error">Error fetching data</Typography>
+            ) : filterReportData && filterReportData.length > 0 ? (
+                <TableCustom title="Report List" data={filterReportData} columns={reportColumns} />
             ) : (
-                <TableCustom title="Badge List" data={badgeData} columns={badgeColumns} />
+                // Display this section if no report match the search query
+                <NoRecordData />
             )}
         </Box>
     );
