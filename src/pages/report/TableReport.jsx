1705d301afefdcf00381199ec7b4c4a604ff903b
Khun Malen <malen8622@gmail.com>
2024-09-23 17:49:20 +0700

    refactor(27-dashbaord): refactor table report

--- src/pages/report/TableReport.jsx
+++ src/pages/report/TableReport.jsx
@@ -7,8 +7,8 @@ import { Box, Typography, CircularProgress } from "@mui/material";
 // Custom Import
 import SearchBarCustom from "../../components/SearchBarCustom";
 import TableCustom from "../../components/TableList";
-import FormatYear from "../../utils/formatDate";
 import NoRecordData from "../../components/NoRecordData";
+
 // Fetching Data Import
 import { useFetchInstitutionStatsQuery } from "../../store/api/reports/institutionStatApis";
 
@@ -20,7 +20,7 @@ const TableReport = () => {
     //Search query state for filtering report data
     const [searchQuery, setSearchQuery] = useState("");
 
-    // Institution Columns
+    // Report Columns
     const reportColumns = [
         {
             name: "Organization",
@@ -44,33 +44,17 @@ const TableReport = () => {
                 row.Issuers.reduce((totalEarners, issuer) => totalEarners + (issuer.Earners?.length || 0), 0),
             sortable: true,
         },
-        {
-            name: "Expire Date",
-            selector: (row) => {
-                const latestBadge = row.Issuers.flatMap((issuer) => issuer.BadgeClasses).reduce((latest, badge) => {
-                    if (!latest || new Date(badge.expiredDate) > new Date(latest.expiredDate)) {
-                        return badge;
-                    }
-                    return latest;
-                }, null);
-
-                return latestBadge ? FormatYear({ dateString: latestBadge.expiredDate }) : "N/A";
-            },
-            sortable: true,
-        },
     ];
 
-    //FilterReport Data = reportData?.
-
+    // FilterReport Data = reportData?.
     const filterReportData = reportData?.filter((report) =>
         report.institutionName.toLowerCase().includes(searchQuery),
     );
 
     return (
         <Box>
-            {/* Search bar */}
             <SearchBarCustom onSearch={setSearchQuery} />
-            {/* Table Data Rendering */}
+            
             {isLoading ? (
                 <CircularProgress />
             ) : isError ? (
@@ -86,3 +70,4 @@ const TableReport = () => {
 };
 
 export default TableReport;
+// ============ End Table Report ============
