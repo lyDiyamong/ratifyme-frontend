885a53f1a1627406f1c13055ab67819ff22f98b9
Khun Malen <malen8622@gmail.com>
2024-09-23 15:09:00 +0700

    add(27-dashbaord): create new component for no record data when search not match

---
+++ src/components/NoRecordData.jsx
@@ -0,0 +1,20 @@
+import { Box, CardMedia, Typography } from "@mui/material";
+import StatusCode from "../assets/images/NoData.svg";
+
+const NoRecordData = () => {
+    return (
+        <Box display="flex" flexDirection="column" alignItems="center" p={4}>
+            <CardMedia
+                component="img"
+                image={StatusCode}
+                alt="No earners found"
+                sx={{ maxWidth: 400, width: "100%" }}
+            />
+            <Typography variant="h6" mt={2} textAlign="center" color="text.secondary">
+                No earners found. Try adjusting your search criteria.
+            </Typography>
+        </Box>
+    );
+};
+
+export default NoRecordData;
