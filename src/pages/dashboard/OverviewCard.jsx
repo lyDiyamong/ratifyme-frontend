e0c1f00afaacc9888eaec850bb425aed12bbfcf2
Khun Malen <malen8622@gmail.com>
2024-09-23 17:47:37 +0700

    add(27-dashbaord): Fetching data to all the card overview in dashboard totol Institution, total Issuer and total earner

--- src/pages/dashboard/OverviewCard.jsx
+++ src/pages/dashboard/OverviewCard.jsx
@@ -1,15 +1,67 @@
-//MUI Import
-import { CardMedia, Typography, Box } from "@mui/material";
+// React Library
+import { useEffect, useState } from "react";
 
-//Custom Import
-import { cardContents } from "../../data/dashboardPage/cardOverviewData";
+// MUI Import
+import { Box, Typography, CircularProgress, CardMedia } from "@mui/material";
+
+// Custom Import
+import blueGraphSvg from "../../assets/images/bluegraph.svg";
+import purpleGraphSvg from "../../assets/images/purplegraph.svg";
+import yellowGraphSvg from "../../assets/images/yellowgraph.svg";
+import blueArrowSvg from "../../assets/icons/bluearrow.svg";
+import purpleArrowSvg from "../../assets/icons/purplearrow.svg";
+import yellowgraphArrowSvg from "../../assets/icons/yellowarrow.svg";
+
+// Fetching Data
+import { useFetchInstitutionStatsQuery } from "../../store/api/reports/institutionStatApis";
 import theme from "../../assets/themes";
 
-// ============ Start Cards Component ============
+// ============ Start card list dashboard ============
+const createCardContent = (totalInstitutions, totalIssuers, totalEarners) => [
+    {
+        image: blueGraphSvg,
+        title: "Total Institution",
+        icon: blueArrowSvg,
+        value: totalInstitutions,
+    },
+    {
+        image: purpleGraphSvg,
+        title: "Total Issuer",
+        icon: purpleArrowSvg,
+        value: totalIssuers,
+    },
+    {
+        image: yellowGraphSvg,
+        title: "Total Earner",
+        icon: yellowgraphArrowSvg,
+        value: totalEarners,
+    },
+];
+
+// Cards Component
 const CardsList = () => {
+    const { data: response, isLoading, isError } = useFetchInstitutionStatsQuery();
+    const [cardContents, setCardContents] = useState([]);
+
+    useEffect(() => {
+        if (response) {
+            const totalInstitutions = response.data.length;
+            const totalIssuers = response.data.reduce((total, institution) => total + institution.Issuers.length, 0);
+            const totalEarners = response.data.reduce(
+                (total, institution) =>
+                    total +
+                    institution.Issuers.reduce((issuerTotal, issuer) => issuerTotal + (issuer.Earners?.length || 0), 0),
+                0,
+            );
+
+            setCardContents(createCardContent(totalInstitutions, totalIssuers, totalEarners));
+        }
+    }, [response]);
+
+    if (isLoading) return <CircularProgress />;
+    if (isError) return <Typography color="error">Error fetching data</Typography>;
 
     return (
-        //============ Start Cards Container  ============
         <Box
             sx={{
                 display: "flex",
@@ -20,10 +72,9 @@ const CardsList = () => {
                 },
             }}
         >
-            {/* ============ Start Card Overview ============*/}
-            {cardContents.map((card) => (
+            {cardContents.map(({ id, image, title, icon, value }) => (
                 <Box
-                    key={card.id}
+                    key={id}
                     sx={{
                         flex: 1,
                         p: 3,
@@ -43,53 +94,25 @@ const CardsList = () => {
                         backgroundColor: theme.palette.customColors.white,
                     }}
                 >
-                    
-                    {/* Image graph */}
-                    <CardMedia
-                        component="img"
-                        image={card.image}
-                        sx={{
-                            maxWidth: 100,
-                            width: "100%",
-                        }}
-                    />
+                    <CardMedia component="img" image={image} sx={{ maxWidth: 100, width: "100%" }} />
                     <Box>
-                        {/* Display Number Data */}
                         <Typography
                             variant="h2"
-                            sx={{
-                                color: theme.palette.text.disabled,
-                                fontWeight: theme.fontWeight.semiBold,
-                            }}
+                            sx={{ color: theme.palette.text.disabled, fontWeight: theme.fontWeight.semiBold }}
                         >
-                            {card.value}
-                            <Box
-                                component="img"
-                                src={card.icon}
-                                sx={{
-                                    mb: "22px",
-                                    "@media (max-width: 650px)": { mb: "12px" },
-                                }}
-                            />
+                            {value}
+                            <Box component="img" src={icon} sx={{ mb: { xs: "24px", xss: "16px" } }} />
                         </Typography>
-                        {/* Title Card */}
-                        <Typography
-                            variant="body2"
-                            sx={{
-                                color: theme.palette.text.primary,
-                                "@media (max-width: 600px)": { fontSize: theme.typography.body3 },
-                            }}
-                        >
-                            {card.title}
+
+                        <Typography variant="body3" sx={{ color: theme.palette.text.primary }}>
+                            {title}
                         </Typography>
                     </Box>
                 </Box>
             ))}
-            {/* ============ End Card Overview ============*/}
         </Box>
-        //============ Start Cards Container  ============
     );
 };
-// ============ End Cards Component ============
 
 export default CardsList;
+// ============ End card list dashboard ============
