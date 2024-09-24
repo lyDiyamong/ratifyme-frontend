38d04ad688b187518e138b04ee106cb7c50a4684
Sreypovprak <sreypovprak88@gmail.com>
2024-09-24 17:15:23 +0700

    refactor(30-signupIntegration): modified stepper flow in signup page

--- src/pages/dashboard/IssuerCard.jsx
+++ src/pages/dashboard/IssuerCard.jsx
@@ -6,28 +6,40 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ClassroomImage from "../../assets/images/classroom.jpg"; // Correctly imported image
+import theme from "../../assets/themes";
+import { useFetchEarnerQuery } from "../../store/api/earnerManagement/earnerApis";
 
 export default function IssuerCard() {
+    const { data: response, isLoading, isError } = useFetchEarnerQuery();
+    const earnerData = response?.data;
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                height="140"
-                image={ClassroomImage} // Using the imported image here
-                alt="Classroom setting"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Classroom
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    A classroom is a learning space where students and teachers interact and engage in educational activities.
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+        <>
+            <Typography
+                variant="h6"
+                sx={{
+                    fontSize: theme.typography.h4,
+                    fontWeight: theme.fontWeight.bold,
+                    color: theme.palette.text.primary,
+                }}
+            >
+                All Classes
+            </Typography>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia component="img" height="140" image={ClassroomImage} alt="Classroom setting" />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Classroom
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        A classroom is a learning space where students and teachers interact and engage in educational
+                        activities.
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </>
     );
 }
