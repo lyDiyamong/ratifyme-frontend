e05fdb37c576b85dcf565278261acfa7a6d7ec0f
lyDiyamong <diyamong26@gmail.com>
2024-09-25 10:32:43 +0700

    add(38-paymentIntegrate): add comment to the block of code

--- src/pages/servicePlanManagement/index.jsx
+++ src/pages/servicePlanManagement/index.jsx
@@ -1,24 +1,34 @@
-import { Box, Paper } from "@mui/material";
+// MUI import
+import { Box } from "@mui/material";
+
+// Custom import
 import PageTitle from "../../components/PageTitle";
 import ServiceDetail from "../../components/ServiceDetail";
 import DashboardContainer from "../../components/styles/DashboardContainer";
 import theme from "../../assets/themes";
-import shadows from "@mui/material/styles/shadows";
 
 const ServicePlanManagement = () => {
     return (
+        // ============ Start ServicePlanManagement ============
         <DashboardContainer>
+            {/* Page Title component */}
             <PageTitle title="Service Plans" />
-            <Box sx={{
-                bgcolor: theme.palette.customColors.white,
-                mt: 4,
-                p: 4,
-                borderRadius : theme.customShape.section,
-                boxShadow : theme.customShadows.default
-            }} >
+            {/* ServiceDetail container */}
+            <Box
+                component="section"
+                sx={{
+                    bgcolor: theme.palette.customColors.white,
+                    mt: 4,
+                    p: 4,
+                    borderRadius: theme.customShape.section,
+                    boxShadow: theme.customShadows.default,
+                }}
+            >
+                {/* Service detail component */}
                 <ServiceDetail />
             </Box>
         </DashboardContainer>
+        // ============ End ServicePlanManagement ============
     );
 };
 
