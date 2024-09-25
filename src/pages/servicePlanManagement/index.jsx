39cdf5109d593b9cf8a7ae2dc59e2f776aba9abd
lyDiyamong <diyamong26@gmail.com>
2024-09-25 10:25:07 +0700

    add(38-paymentIntegrate): add serviceDetail to admin dashboard

--- src/pages/servicePlanManagement/index.jsx
+++ src/pages/servicePlanManagement/index.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Box, Paper } from "@mui/material";
+import PageTitle from "../../components/PageTitle";
+import ServiceDetail from "../../components/ServiceDetail";
+import DashboardContainer from "../../components/styles/DashboardContainer";
+import theme from "../../assets/themes";
+import shadows from "@mui/material/styles/shadows";
 
 const ServicePlanManagement = () => {
-    return <div>ServicePlanManagement</div>;
+    return (
+        <DashboardContainer>
+            <PageTitle title="Service Plans" />
+            <Box sx={{
+                bgcolor: theme.palette.customColors.white,
+                mt: 4,
+                p: 4,
+                borderRadius : theme.customShape.section,
+                boxShadow : theme.customShadows.default
+            }} >
+                <ServiceDetail />
+            </Box>
+        </DashboardContainer>
+    );
 };
 
 export default ServicePlanManagement;
