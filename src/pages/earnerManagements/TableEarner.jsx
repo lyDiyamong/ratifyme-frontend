bd3d65a33ab5c553d2025e8454bee81ce083c95e
Khun Malen <malen8622@gmail.com>
2024-09-26 10:35:30 +0700

    refactor(40-tableReusable): refactor table earner to dynamic base on the role

--- src/pages/earnerManagements/TableEarner.jsx
+++ src/pages/earnerManagements/TableEarner.jsx
@@ -2,21 +2,23 @@
 import { useState } from "react";
 
 // MUI Import
-import { Box, Typography, CircularProgress, CardMedia } from "@mui/material";
+import { Box, Typography, CircularProgress } from "@mui/material";
 
 // Custom Import
-
 import SearchBarCustom from "../../components/SearchBarCustom";
 import TableCustom from "../../components/TableList";
 import MenuSelection from "../../components/TableAction/MenuSelection";
 import FormatYear from "../../utils/formatDate";
 import ProfileEarnerModal from "./ProfileEarnerModal";
 import NoRecordData from "../../components/NoRecordData";
+
 // Fetching Data Import
 import { useFetchEarnerQuery, useDeleteEarnerByIdMutation } from "../../store/api/earnerManagement/earnerApis";
+import { useCheckAuthQuery } from "../../store/api/auth/authApi";
 
-// ============ Start Table Earner Modal Custom Button ============
+// ============ Start Table Earner Modal ============
 const TableEarner = () => {
+    const { data: user } = useCheckAuthQuery();
     const { data: response, isLoading, isError } = useFetchEarnerQuery();
     const [deleteEarner] = useDeleteEarnerByIdMutation();
 
@@ -27,7 +29,30 @@ const TableEarner = () => {
     // Search query state for filtering earner data
     const [searchQuery, setSearchQuery] = useState("");
 
+    // Earner data fetched from the API
     const earnerData = response?.data;
+
+    //Check auth for the specific role
+    const roleId = user?.user?.roleId;
+
+    //Receive the userID
+    const { id: userID } = user.user;
+
+    // Display earner in the earner table by the specific role , role = 1 (Admin), role = 2 (institutionOwner), role = 3 (issuer)
+    let filteredEarnerData;
+
+    if (roleId === 1) {
+        filteredEarnerData = earnerData;
+    } else if (roleId === 2) {
+        filteredEarnerData = earnerData?.filter((earner) => earner.Issuer?.Institution?.userId === userID);
+    } else if (roleId === 3) {
+        filteredEarnerData = earnerData?.filter((earner) => earner.Issuer?.userId === userID);
+    }
+
+    // Further filter based on the search query (optional)
+    const searchedEarnerData = filteredEarnerData?.filter((earner) =>
+        earner.User.username.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
 
     // Handle View (open the modal)
     const handleView = (userId) => {
@@ -54,22 +79,22 @@ const TableEarner = () => {
     const earnerColumns = [
         {
             name: "ID",
-            selector: (row) => row.id || 'N/A',
+            selector: (row) => row.id || "N/A",
             sortable: true,
         },
         {
             name: "Name",
-            selector: (row) => row.User?.username || 'N/A',
+            selector: (row) => row.User?.username || "N/A",
             sortable: true,
         },
         {
             name: "Email",
-            selector: (row) => row.User?.email || 'N/A',
+            selector: (row) => row.User?.email || "N/A",
             sortable: true,
         },
         {
             name: "Badge",
-            selector: (row) => row.Achievement?.BadgeClass?.name || 'N/A',
+            selector: (row) => row.Achievement?.BadgeClass?.name || "N/A",
             sortable: true,
         },
         {
@@ -85,14 +110,6 @@ const TableEarner = () => {
         },
     ];
 
-    // Filter data based on the search query
-    const filteredEarnerData = earnerData?.filter(
-        (earner) =>
-            earner.User.username.toLowerCase().includes(searchQuery) ||
-            earner.User.email.toLowerCase().includes(searchQuery) ||
-            earner.Achievement.BadgeClass.name.toLowerCase().includes(searchQuery),
-    );
-
     return (
         <Box>
             {/* Search Bar */}
@@ -106,15 +123,15 @@ const TableEarner = () => {
                 <CircularProgress />
             ) : isError ? (
                 <Typography color="error">Error fetching data</Typography>
-            ) : filteredEarnerData && filteredEarnerData.length > 0 ? (
-                <TableCustom title="Earner List" data={filteredEarnerData} columns={earnerColumns} />
+            ) : searchedEarnerData && searchedEarnerData.length > 0 ? (
+                <TableCustom title="Earner List" data={searchedEarnerData} columns={earnerColumns} />
             ) : (
                 // Display this section if no earners match the search query
-                <NoRecordData/>
+                <NoRecordData />
             )}
         </Box>
     );
 };
 
 export default TableEarner;
-// ============ End Table Earner Modal Custom Button ============
+// ============ End Table Earner Modal ============
