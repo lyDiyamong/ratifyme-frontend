59f01e42dcd773061c9537083d2d3db499071a03
lyDiyamong <diyamong26@gmail.com>
2024-09-26 20:05:25 +0700

    add(38-paymentIntegrate): add badges list to institution detail

--- src/pages/institutionDetail/index.jsx
+++ src/pages/institutionDetail/index.jsx
@@ -10,6 +10,9 @@ import AlertMessage from "../../components/alert/AlertMessage";
 // Api import
 import { useGetInstitutionByIdQuery } from "../../store/api/institutionManagement/institutionApi";
 import BadgeListCard from "../../components/BadgeListCard";
+import theme from "../../assets/themes";
+import { Box } from "@mui/material";
+import { useFetchBadgesByIssuerQuery } from "../../store/api/badgeManagement/badgeApi";
 
 function InstitutionDetail() {
     // Error state hook
@@ -18,10 +21,13 @@ function InstitutionDetail() {
     // Get the instituion id hook
     const { institutionId } = useParams();
 
-    // Api fetching hook
+    // Api fetching institution hook
     const { data: response, isLoading, isError } = useGetInstitutionByIdQuery(institutionId);
     const institution = response?.data;
 
+    // Api fetching badges hook
+    const { data: badges } = useFetchBadgesByIssuerQuery();
+    console.log(badges?.data || {});
     // Error handling
     if (isError) {
         setErrorMessage("There was an error fetching subscription data. Please try again later.");
@@ -37,7 +43,21 @@ function InstitutionDetail() {
                 logoUrl={institution?.User.profileImage}
             />
             {/* Badge List */}
-            <BadgeListCard />
+            <Box
+                component="section"
+                sx={{
+                    boxShadow: theme.customShadows.default,
+                    borderRadius: theme.customShape.section,
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    bgcolor: theme.palette.customColors.white,
+                    px: 2,
+                    py: 2,
+                    mt: 4,
+                }}
+            >
+                {/* <BadgeListCard badges={badges?.data} /> */}
+            </Box>
         </DashboardContainer>
         // ============ End InstitutionDetail ============
     );
