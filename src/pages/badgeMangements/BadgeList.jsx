08b80984ffef5908688ca2b265737c22deb1b478
Lyhour12 <slyhour69@gmail.com>
2024-09-27 13:44:13 +0700

    add(35-badgeDetailIntegrate): create an api to fetch badge class from issuer and institution

--- src/pages/badgeMangements/BadgeList.jsx
+++ src/pages/badgeMangements/BadgeList.jsx
@@ -1,42 +1,43 @@
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 // MUI import
 import { Typography } from "@mui/material";
 
 // Custom import
-import { useFetchBadgesByIssuerQuery } from "../../store/api/badgeManagement/badgeApi";
-import { useCheckAuthQuery } from "../../store/api/auth/authApi";
+import {
+    useFetchBadgesByIssuerQuery,
+    useFetchBadgesByInstitutionsQuery,
+    useFetchBadgesQuery,
+} from "../../store/api/badgeManagement/badgeApi";
 import BadgeListCard from "../../components/BadgeListCard";
 
 const BadgeList = () => {
     const navigate = useNavigate();
-    const { data: user, isLoading: isAuthLoading } = useCheckAuthQuery();
-    const { data, isLoading, isError } = useFetchBadgesByIssuerQuery();
-    const badges = data?.data || [];
+    const { roleId, issuerData, institutionData } = useSelector((state) => state.global);
 
-    const userId = user?.user?.id;
-    const roleId = user?.user?.roleId;
-    const institutionId = badges.length > 0 ? badges[0]?.Issuer?.institutionId : null;
+    // Determine the realId based on the role
+    const activeId = roleId === 2 ? institutionData.id : roleId === 3 ? issuerData.id : "";
 
-    console.log(userId, roleId, institutionId);
+    // Fetch data
+    const { data: allBadges, isLoading, isError } = useFetchBadgesQuery();
+    const { data: badgeInsti } = useFetchBadgesByInstitutionsQuery(activeId);
+    const { data: badgeIssuer } = useFetchBadgesByIssuerQuery(activeId);
 
-    // Apply different filtering based on role
-    let checkBadge = [];
+    // Define badges based on role
+    const badges = allBadges?.data || [];
+    const badgeInstitution = badgeInsti?.data?.Issuers.flatMap((issuer) => issuer.BadgeClasses) || [];
+    const badgeIssue = badgeIssuer?.data?.BadgeClasses || [];
 
-    if (roleId === 3) {
-        // Issuer - Only view their own badges
-        checkBadge = badges.filter((badge) => badge?.Issuer?.userId === userId);
-    } else if (roleId === 2) {
-        // Institution - View all badges of issuers within the same institution
-        checkBadge = badges.filter((badge) => badge?.Issuer?.Institution?.id === institutionId);
-    } else if (roleId === 1) {
-        // Admin - View all badges
-        checkBadge = badges; // Admin can see all badges
-    }
-
-    console.log(checkBadge);
+    // Apply filtering based on role
+    const checkBadge =
+        roleId === 3
+            ? badgeIssue 
+            : roleId === 2
+            ? badgeInstitution
+            : badges; 
 
     // Handle loading, error, and empty state in the parent component
-    if (isLoading || isAuthLoading) return <Typography>Loading...</Typography>;
+    if (isLoading) return <Typography>Loading...</Typography>;
     if (isError) return <Typography>Error fetching badges</Typography>;
 
     // Handle view badgeDetail
