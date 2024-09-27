1d2184c6c76709e02b9a3c6150dc3473839a084b
Sreypovprak <sreypovprak88@gmail.com>
2024-09-27 14:36:16 +0700

    fix(41-inviteUserIntegration): resolve conflict

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,7 @@ import { verificationApi } from "./api/earnerManagement/verificationApi";
 
 import { setupListeners } from "@reduxjs/toolkit/query";
 import gloableReducer from "./slices/globalSlices";
+import { userApi } from "./api/users/userProfileApi";
 
 const store = configureStore({
     reducer: {
