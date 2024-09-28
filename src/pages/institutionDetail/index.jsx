88911d22294eba3a0b2bcd48ed4f8401102a2ff1
lyDiyamong <diyamong26@gmail.com>
2024-09-28 10:57:21 +0700

    add(38-paymentIntegrate): add badge list for each institution profile

--- src/pages/institutionDetail/index.jsx
+++ src/pages/institutionDetail/index.jsx
@@ -1,6 +1,6 @@
 // React import
 import { useState } from "react";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 // Custom import
 import OrganizationCard from "../../components/OrganizationCard";
@@ -8,17 +8,19 @@ import DashboardContainer from "../../components/styles/DashboardContainer";
 import AlertMessage from "../../components/alert/AlertMessage";
 import { SpinLoading } from "../../components/loading/SpinLoading";
 
-
 // Api import
 import { useGetInstitutionByIdQuery } from "../../store/api/institutionManagement/institutionApi";
 import BadgeListCard from "../../components/BadgeListCard";
 import theme from "../../assets/themes";
 import { Box } from "@mui/material";
-import { useFetchBadgesByIssuerQuery } from "../../store/api/badgeManagement/badgeApi";
+import { useFetchBadgesByInstitutionsQuery } from "../../store/api/badgeManagement/badgeApi";
 
 function InstitutionDetail() {
     // Error state hook
     const [errorMessage, setErrorMessage] = useState("");
+
+    // Navigation hook
+    const navigate = useNavigate()
 
     // Get the instituion id hook
     const { institutionId } = useParams();
@@ -28,8 +30,16 @@ function InstitutionDetail() {
     const institution = response?.data;
 
     // Api fetching badges hook
-    const { data: badges, isLoading : badgesLoading } = useFetchBadgesByIssuerQuery();
-    console.log(badges?.data || {});
+    const { data: badgesIntsti, isLoading: badgesLoading } = useFetchBadgesByInstitutionsQuery(institutionId);
+    console.log(badgesIntsti?.data?.Issuers);
+
+    // Using flatMap to iterating to array in array
+    const badges = badgesIntsti?.data?.Issuers?.flatMap((badge) => badge?.BadgeClasses);
+
+    const handleView = (id) => {
+        navigate(`/management/badges/badgeDetail/${id}`);
+    };
+
     // Error handling
     if (isError) {
         setErrorMessage("There was an error fetching subscription data. Please try again later.");
@@ -58,8 +68,7 @@ function InstitutionDetail() {
                     mt: 4,
                 }}
             >
-
-                { badgesLoading  ?  <SpinLoading /> :  <BadgeListCard badges={badges?.data} />}
+                {badgesLoading ? <SpinLoading /> : <BadgeListCard badges={badges} onView={handleView} />}
             </Box>
         </DashboardContainer>
         // ============ End InstitutionDetail ============
