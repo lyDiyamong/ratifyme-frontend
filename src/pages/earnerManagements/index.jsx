bcdfabf023311e4282320e84133fffb9b019a72c
KhunMalen <maalen231219@gmail.com>
2024-09-30 13:38:02 +0700

    refactor(44-verifyModal): add new component page into earner management

--- src/pages/earnerManagements/index.jsx
+++ src/pages/earnerManagements/index.jsx
@@ -7,14 +7,15 @@ import DashboardContainer from "../../components/styles/DashboardContainer";
 // Custom Import
 import TableEarner from "./TableEarner";
 import SearchBar from "../../components/SearchBars/SearchBar";
+import VerificationCheckUp from "./verificationCheckUp";
 
 // ============ Start EarnerManagement ============
 const EarnerManagement = () => {
     const [searchQuery, setSearchQuery] = useState("");
     return (
         <DashboardContainer>
-            {/* <SearchBarIssuer onSearch={setSearchQuery} /> */}
             <SearchBar onSearch={setSearchQuery} />
+            <VerificationCheckUp  />
             <TableEarner searchQuery={searchQuery} />
         </DashboardContainer>
     );
