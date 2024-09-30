425f5f0e7c9327c50a71224a17037972e672d719
Lyhour12 <slyhour69@gmail.com>
2024-09-30 17:13:43 +0700

    change(35-badgeDetailIntegrate): add component that show only badge has sent

--- src/pages/backpackManagement/index.jsx
+++ src/pages/backpackManagement/index.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import DashboardContainer from "../../components/styles/DashboardContainer";
+import PageTitle from "../../components/PageTitle";
+import SearchBar from "../../components/SearchBar";
+import BadgeList from "../badgeMangements/BadgeList";
 
 const BackpackManagement = () => {
-    return <div>BackpackManagement</div>;
+    return (
+        <DashboardContainer sx={{ pb: 4 }}>
+            <PageTitle title="BackPack Management" />
+            <SearchBar showButton={false} textInButton="Add Badge">
+                <BadgeList />
+            </SearchBar>
+        </DashboardContainer>
+    );
 };
 
 export default BackpackManagement;
