83b3addaa1c9b1005314cd031c7b7c4eeba1f963
Sreypovprak <sreypovprak88@gmail.com>
2024-10-04 20:37:29 +0700

    feat(49-forgotPasswordIntegreation): initial forgot password page

---
+++ src/pages/auth/ForgotPasswordPage.jsx
@@ -0,0 +1,195 @@
+// React library import
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate, Link } from "react-router-dom";
+
+// MUI import
+import { Box, Typography, Button, Checkbox, FormControlLabel, IconButton, Divider } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import Google from "@mui/icons-material/Google";
+
+// Custom import
+import FormInput from "../../components/FormInput";
+import theme from "../../assets/themes";
+import RatifyMELogo from "../../assets/icons/RatfiyME.svg";
+import { SpinLoading } from "../../components/loading/SpinLoading";
+import { useSignInMutation } from "../../store/api/auth/authApi";
+import { Stack } from "@mui/system";
+import LockOpenOutlined from "@mui/icons-material/LockOpenOutlined";
+import EmailOutlined from "@mui/icons-material/EmailOutlined";
+import OutletImageComponent from "./OutletImageTemplate";
+
+const ForgotPasswordPage = () => {
+    const [signIn, { isLoading, isError, error }] = useSignInMutation();
+
+    const navigate = useNavigate();
+    // Form controller
+    const {
+        handleSubmit,
+        control,
+        formState: { errors },
+        reset,
+    } = useForm();
+
+    const [loading, setLoading] = useState(false);
+
+    // Submit handler
+    const onSubmit = async (data) => {
+        try {
+            console.log('forgot password')
+        } catch (error) {
+            console.error("Error during sign in:", error.message || error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        // ============ Start login container ============
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ height: "100vh", display: "flex" }}>
+            <Box
+                flexGrow={0}
+                display="flex"
+                justifyContent="center"
+                sx={{
+                    borderRadius: theme.customShape.section,
+                    width: { md: "50%", xss: "100%" },
+                    mx: "auto",
+                    px: 4,
+                    backgroundColor: "transparent",
+                }}
+            >
+                <Stack width="100%" maxWidth="450px" gap={2}>
+                    <Link to="/">
+                        <Box
+                            component="img"
+                            src={RatifyMELogo}
+                            alt="Ratifyme Favicon"
+                            sx={{ width: 150, height: 150 }}
+                        />
+                    </Link>
+
+                    <Box my={3}>
+                        <Typography variant="h3" fontWeight={theme.fontWeight.semiBold} mb={1}>
+                            Welcome to RatifyME! 👋
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" mb={3}>
+                            Please sign-in to your account and start the adventure.
+                        </Typography>
+                    </Box>
+
+                    <Stack spacing={2}>
+                        {/* Email */}
+                        <FormInput
+                            name="email"
+                            type="email"
+                            control={control}
+                            label="Email"
+                            required={true}
+                            startIcon={<EmailOutlined />}
+                        />
+                        {/* Password */}
+                        <FormInput
+                            name="password"
+                            control={control}
+                            label="Password"
+                            type="password"
+                            required={true}
+                            startIcon={<LockOpenOutlined />}
+                        />
+                    </Stack>
+
+                    <Stack
+                        flexDirection={{ xs: "row", xss: "column" }}
+                        alignItems={{ xs: "center", xss: "start" }}
+                        justifyContent="space-between"
+                    >
+                        <FormControlLabel
+                            sx={{ color: theme.palette.text.secondary }}
+                            control={<Checkbox />}
+                            label="Remember Me"
+                        />
+                        <Link>
+                            <Typography
+                                component="a"
+                                href="#"
+                                variant="body1"
+                                color="primary"
+                                sx={{ textDecoration: "none" }}
+                            >
+                                Forgot password?
+                            </Typography>
+                        </Link>
+                    </Stack>
+
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        type="submit"
+                        size="large"
+                        disabled={loading}
+                        sx={{
+                            color: theme.palette.customColors.white,
+                            fontWeight: theme.fontWeight.bold,
+                            borderRadius: theme.customShape.btn,
+                        }}
+                    >
+                        {loading ? <SpinLoading size={24} /> : "Sign in"}
+                    </Button>
+
+                    <Divider sx={{ my: 4, color: theme.palette.text.secondary }}>Or</Divider>
+
+                    <Box display="flex" justifyContent="center" gap={1}>
+                        <IconButton
+                            sx={{
+                                color: theme.palette.primary.dark,
+                                backgroundColor: theme.palette.primary.light,
+                                borderRadius: theme.customShape.input,
+                            }}
+                        >
+                            <FacebookIcon />
+                        </IconButton>
+                        <IconButton
+                            sx={{
+                                color: theme.palette.customColors.orange200,
+                                backgroundColor: theme.palette.background.error,
+                                borderRadius: theme.customShape.input,
+                            }}
+                        >
+                            <Google />
+                        </IconButton>
+                        <IconButton
+                            sx={{
+                                color: theme.palette.secondary.light,
+                                backgroundColor: theme.palette.primary.light,
+                                borderRadius: theme.customShape.input,
+                            }}
+                        >
+                            <TwitterIcon />
+                        </IconButton>
+                    </Box>
+
+                    <Typography variant="body2" align="center" color="text.secondary" mt={2}>
+                        New on our platform?{" "}
+                        <Link to="/get-started">
+                            <Typography
+                                component="a"
+                                href="#"
+                                variant="body2"
+                                color="primary"
+                                sx={{ textDecoration: "none" }}
+                            >
+                                Create an account
+                            </Typography>
+                        </Link>
+                    </Typography>
+                </Stack>
+            </Box>
+
+        </Box>
+        // ============ End login container ============
+    );
+};
+
+export default ForgotPasswordPage;
