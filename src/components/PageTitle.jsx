c8d7e163f9f4bd0d150ec0d2a46fdc077d53951e
PrakSreypov <129585132+PrakSreypov@users.noreply.github.com>
2024-10-04 09:17:03 +0700

    styles(demoAuth): modified syles of Page Title page

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,9 +1,8 @@
 // MUI import
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 // Custom import
 import theme from "../assets/themes";
-
 
 // Start current date function
 const getCurrentDate = () => {
@@ -19,7 +18,7 @@ const getCurrentDate = () => {
  * @param {Object} sx - Additional style mui
  * @returns {JSX.Element} The rendered PageTitle component.
  */
-const PageTitle = ({ title, sx }) => {
+const PageTitle = ({ title, sx, subtitle }) => {
     return (
         // ============ Start Page Title ============
         <Box
@@ -27,22 +26,36 @@ const PageTitle = ({ title, sx }) => {
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center",
-                marginTop: 2,
+                my: 5,
                 bgcolor: theme.palette.background.default,
-                ...sx
+                ...sx,
             }}
         >
-            {/* Start Page Title */}
-            <Typography
-                variant="h4"
-                sx={{
-                    fontSize: theme.typography.h3,
-                    fontWeight: theme.fontWeight.bold,
-                    color: theme.palette.text.primary,
-                }}
-            >
-                {title}
-            </Typography>
+            <Stack direction="column">
+                {/* Start Page Title */}
+                <Typography
+                    variant="h2"
+                    sx={{
+                        fontSize: theme.typography.h2,
+                        fontWeight: theme.fontWeight.bold,
+                        color: theme.palette.text.primary,
+                        lineHeight: 1.8
+                    }}
+                >
+                    {title}
+                </Typography>
+
+                {/* Start Page Sub title  */}
+                <Typography
+                    variant="h4"
+                    sx={{
+                        fontSize: theme.typography.body2,
+                        color: theme.palette.text.secondary,
+                    }}
+                >
+                    {subtitle}
+                </Typography>
+            </Stack>
 
             {/* Start Current Date */}
             <Typography variant="body2" sx={{ color: theme.palette.customColors.gray300 }}>
