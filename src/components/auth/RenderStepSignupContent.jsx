ac8963782cb378fdcbecda574226e5f78ae49db0
Sreypovprak <sreypovprak88@gmail.com>
2024-10-08 20:00:38 +0700

    add(51-enhanceSignupPage): add RenderStepSignupContent as component

---
+++ src/components/auth/RenderStepSignupContent.jsx
@@ -0,0 +1,29 @@
+// SignupStepContent.js
+
+import GeneralInfoFields from "./GeneralInfoFields";
+import AddressFields from "./AddressFields";
+import InstitutionInfoFields from "./InstitutionInfoFields";
+import AccountSetupFields from "./AccountSetupFields";
+
+const RenderStepSignupContent = ({ step, control, role, guest, watch }) => {
+    switch (step) {
+        case 0:
+            return <GeneralInfoFields control={control} />;
+        case 1:
+            return <AddressFields control={control} />;
+        case 2:
+            return role === "institution" ? (
+                <InstitutionInfoFields control={control} />
+            ) : (
+                <AccountSetupFields control={control} role={role} guest={guest} watch={watch} />
+            );
+        case 3:
+            return role === "institution" ? (
+                <AccountSetupFields control={control} role={role} guest={guest} watch={watch} />
+            ) : null;
+        default:
+            return null;
+    }
+};
+
+export default RenderStepSignupContent;
