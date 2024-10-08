2d9b208c330e40289d76c54c47b9b1f1cf7cbb74
RothaSAMON <samonrotha@gmail.com>
2024-10-08 13:27:28 +0700

    style(47-certPdf): added style to certificate and added explosion library

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -2,7 +2,7 @@
 import { forwardRef } from "react";
 
 // MUI import
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 
 // Custom import
 import theme from "../assets/themes";
@@ -16,118 +16,146 @@ const Certificate = forwardRef(({ recipientName, date, badge }, ref) => (
     <Stack
         ref={ref}
         sx={{
-            maxWidth: "1000px",
+            minWidth: "1000px",
             height: 700,
-            width: "100%",
             border: "10px solid #ffffff",
             padding: 3,
-            alignItems: "center",
+            gap: 3,
             position: "relative",
             textAlign: "center",
             backgroundColor: theme.palette.customColors.white,
-            backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url(${certWave})`, // Apply a transparent overlay
+            backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url(${certWave})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             justifyContent: "center",
         }}
     >
-        <Box
-            sx={{
-                display: "flex",
-                gap: 2,
-                alignItems: "center",
-            }}
-        >
-            <Typography
-                sx={{
-                    color: theme.palette.text.primary,
-                    letterSpacing: 2,
-                    fontSize: theme.typography.h2,
-                    lineHeight: 2,
-                    fontWeight: theme.fontWeight.bold,
-                    textTransform: "uppercase",
-                }}
-            >
-                ABOVE AND BEYOND SCHOOL
-            </Typography>
-        </Box>
+        <Stack sx={{ flexDirection: "row", gap: 1, alignItems: "center" }}>
+            <Box
+                component="img"
+                src="https://ratifyme.s3.ap-southeast-2.amazonaws.com/Logo/RatifyME-Fevicon.svg"
+                alt="RatifyMe"
+            />
+            <Divider sx={{ height: 45, borderColor: "black" }} orientation="vertical" />
+            <Stack justifyContent="start" alignItems="start">
+                <Typography variant="h3" fontWeight={theme.fontWeight.semiBold}>
+                    RatifyMe
+                </Typography>
+                <Typography variant="body2" >
+                    RatifyMe by{" "}
+                    <span style={{ color: theme.palette.primary.main, fontWeight: theme.fontWeight.semiBold }}>
+                        TechA
+                    </span>
+                </Typography>
+            </Stack>
+        </Stack>
 
-        <Box
-            sx={{
-                mt: 2,
-            }}
-        >
-            <Typography
+        <Stack>
+            <Box
                 sx={{
-                    color: theme.palette.text.disabled,
-                    letterSpacing: 2,
-                    fontSize: theme.typography.body2,
-                    lineHeight: 2,
-                    fontWeight: theme.fontWeight.bold,
+                    display: "flex",
+                    gap: 2,
+                    alignItems: "center",
+                    justifyContent: "center",
                 }}
             >
-                Certifies that
-            </Typography>
-            {/* Badge name */}
-            <Typography variant="h2" fontWeight="bold" gutterBottom>
-                {recipientName}
-            </Typography>
-
-            <Typography
-                sx={{
-                    color: theme.palette.text.disabled,
-                    letterSpacing: 2,
-                    fontSize: theme.typography.body2,
-                    lineHeight: 2,
-                    fontWeight: theme.fontWeight.bold,
-                }}
-            >
-                successfully completed all requirements in
-            </Typography>
-            <Typography
-                sx={{
-                    fontSize: theme.typography.h1,
-                    color: theme.palette.text.primary,
-                    fontWeight: theme.fontWeight.bold,
-                    lineHeight: 2,
-                }}
-            >
-                {badge?.name}
-            </Typography>
-            <Box component="img" src="https://ratifyme.s3.ap-southeast-2.amazonaws.com/Badges/Bagde+Image.png" alt={badge?.name || "Badge"} maxWidth={100} maxHeight={100} />
-            <Typography
-                sx={{
-                    fontSize: theme.typography.body2,
-                    color: theme.palette.text.disabled,
-                    lineHeight: 2,
-                }}
-            >
-                This badge certifies advanced knowledge of web development.
-            </Typography>
-            {/* Issuer name */}
-            <Typography
-                sx={{
-                    fontSize: theme.typography.body2,
-                }}
-            >
-                Issuer:{" "}
                 <Typography
-                    component="span"
                     sx={{
-                        fontSize: theme.typography.body2,
                         color: theme.palette.text.primary,
+                        letterSpacing: 2,
+                        fontSize: theme.typography.h2,
+                        lineHeight: 2,
+                        fontWeight: theme.fontWeight.bold,
+                        textTransform: "uppercase",
+                    }}
+                >
+                    ABOVE AND BEYOND SCHOOL
+                </Typography>
+            </Box>
+
+            <Box
+                sx={{
+                    mt: 2,
+                }}
+            >
+                <Typography
+                    sx={{
+                        color: theme.palette.text.disabled,
+                        letterSpacing: 2,
+                        fontSize: theme.typography.body2,
+                        lineHeight: 2,
                         fontWeight: theme.fontWeight.bold,
                     }}
                 >
-                    {badge?.Issuer?.User?.name || "Mary Jane"}
+                    Certifies that
                 </Typography>
-            </Typography>
-        </Box>
-        <Box sx={{ mt: 3 }}>
-            <Typography variant="body1" sx={{ textAlign: "center" }}>
-                Issued On: {date}
-            </Typography>
-        </Box>
+                {/* Badge name */}
+                <Typography variant="h2" fontWeight="bold" gutterBottom>
+                    {recipientName}
+                </Typography>
+
+                <Typography
+                    sx={{
+                        color: theme.palette.text.disabled,
+                        letterSpacing: 2,
+                        fontSize: theme.typography.body2,
+                        lineHeight: 2,
+                        fontWeight: theme.fontWeight.bold,
+                    }}
+                >
+                    successfully completed all requirements in
+                </Typography>
+                <Typography
+                    sx={{
+                        fontSize: theme.typography.h1,
+                        color: theme.palette.text.primary,
+                        fontWeight: theme.fontWeight.bold,
+                        lineHeight: 2,
+                    }}
+                >
+                    {badge?.name}
+                </Typography>
+                <Box
+                    component="img"
+                    src="https://ratifyme.s3.ap-southeast-2.amazonaws.com/Badges/GoldBadgeImage.png"
+                    alt={badge?.name || "Badge"}
+                    maxWidth={150}
+                    maxHeight={150}
+                />
+                <Typography
+                    sx={{
+                        fontSize: theme.typography.body2,
+                        color: theme.palette.text.disabled,
+                        lineHeight: 2,
+                    }}
+                >
+                    This badge certifies advanced knowledge of web development.
+                </Typography>
+                {/* Issuer name */}
+                <Typography
+                    sx={{
+                        fontSize: theme.typography.body2,
+                    }}
+                >
+                    Issuer:{" "}
+                    <Typography
+                        component="span"
+                        sx={{
+                            fontSize: theme.typography.body2,
+                            color: theme.palette.text.primary,
+                            fontWeight: theme.fontWeight.bold,
+                        }}
+                    >
+                        {badge?.Issuer?.User?.name || "Mary Jane"}
+                    </Typography>
+                </Typography>
+            </Box>
+            <Box sx={{ mt: 3 }}>
+                <Typography variant="body1" sx={{ textAlign: "center" }}>
+                    Issued On: {date}
+                </Typography>
+            </Box>
+        </Stack>
     </Stack>
 ));
 
--- src/pages/certificateTest/index.jsx
+++ src/pages/certificateTest/index.jsx
@@ -1,80 +1,173 @@
 import { useRef, useState } from "react";
-import html2pdf from "html2pdf.js";
-import { useNavigate } from "react-router";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { toJpeg } from "html-to-image";
+import axios from "axios";
 import BadgeTest from "../../assets/images/badge.png";
 import Certificate from "../../components/Certificate";
-import { toJpeg } from "html-to-image"; // Import html-to-image for conversion
-import axios from "axios";
+import DashboardContainer from "../../components/styles/DashboardContainer";
+import theme from "../../assets/themes";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import ConfettiExplosion from "react-confetti-explosion";
+import DownloadDoneOutlined from "@mui/icons-material/DownloadDoneOutlined";
+import AutoAwesome from "@mui/icons-material/AutoAwesome";
 
 const CertificateGenerator = () => {
     const certificateRef = useRef();
-    const [imageDataUrl, setImageDataUrl] = useState(null);
+    const [pdfUrl, setPdfUrl] = useState("");
+    const [copyButtonText, setCopyButtonText] = useState("Copy URL");
+    const [isExploding, setIsExploding] = useState(false);
 
     const handleGenerateImage = async () => {
         try {
-            // Generate the image from the certificateRef element
-            const svgDataUrl = await toJpeg(certificateRef.current, {
-                quality: 0.95,
-            });
-            setImageDataUrl(svgDataUrl); // Save the image data URL in the state
-            // Convert the SVG Data URL to a Blob
+            const svgDataUrl = await toJpeg(certificateRef.current, { quality: 0.95 });
             const blob = await fetch(svgDataUrl).then((res) => res.blob());
-
-            // Create a FormData object to send the file to the server
             const formData = new FormData();
             formData.append("certFile", blob, "certificate");
 
-            // Send the file to the backend using Axios
             const response = await axios.post(`${import.meta.env.VITE_SERVER_BASE_URL}/earners/uploadCerti`, formData, {
                 headers: { "Content-Type": "multipart/form-data" },
             });
 
-            if (response.data.pdfUrl) {
-                // Get the path without the origin part of the URL
+            console.log(response);
 
-                // Open the PDF in a new tab
+            if (response.data.pdfUrl) {
+                setPdfUrl(response.data.pdfUrl);
                 window.open(response.data.pdfUrl, "_blank");
+            } else {
+                console.error("PDF URL not found in the response.");
             }
         } catch (error) {
             console.error("Error generating and uploading PDF:", error);
         }
     };
 
+    const handleCopyUrl = () => {
+        if (pdfUrl) {
+            navigator.clipboard
+                .writeText(pdfUrl)
+                .then(() => {
+                    setCopyButtonText("Copied!");
+                    setTimeout(() => {
+                        setCopyButtonText("Copy URL");
+                    }, 3000);
+                })
+                .catch((err) => {
+                    console.error("Failed to copy: ", err);
+                });
+        } else {
+            console.error("No URL available to copy.");
+        }
+    };
+
+    const handleCongrats = () => {
+        setIsExploding(true);
+        setTimeout(() => setIsExploding(false), 4000);
+    };
+
     return (
-        <>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <Certificate
-                    ref={certificateRef}
-                    recipientName="Sreang Lyhour"
-                    date="2024-04-27"
-                    badge={{
-                        name: "Advance Frontend",
-                        imageUrl: BadgeTest,
-                    }}
-                />
-            </Box>
-            <Button variant="contained" color="primary" onClick={handleGenerateImage}>
-                Download Certificate
-            </Button>
-            {imageDataUrl && (
+        <DashboardContainer>
+            <Stack
+                sx={{
+                    bgcolor: "white",
+                    borderRadius: theme.customShape.section,
+                    boxShadow: theme.customShadows.default,
+                    gap: 3,
+                    padding: 3,
+                    my: 3,
+                    background: `linear-gradient(to bottom, ${theme.palette.action.hover} 40%, ${theme.palette.customColors.white} 30%)`,
+                }}
+            >
+                <Stack sx={{ justifyContent: "center", alignItems: { md: "center", xss: "start" }, gap: 1 }}>
+                    <Typography variant="h2" sx={{ fontWeight: theme.fontWeight.bold }}>
+                        Here is your certificate!🎉
+                    </Typography>
+                    <Typography variant="body1" sx={{ color: theme.palette.text.secondary }}>
+                        You've worked hard, and it's paid off—congratulations on earning your certificate!
+                    </Typography>
+                </Stack>
+
                 <Box
                     sx={{
-                        mt: 4,
                         display: "flex",
                         justifyContent: "center",
-                        padding: "20px",
+                        width: "100%",
                     }}
                 >
                     <Box
-                        component="img"
-                        src={imageDataUrl}
-                        alt="Generated Certificate"
-                        style={{ maxWidth: "100%", height: "auto" }}
-                    />
+                        sx={{
+                            overflowX: "scroll",
+                            whiteSpace: "nowrap",
+                            "::-webkit-scrollbar": {
+                                display: "none",
+                            },
+                            scrollbarWidth: "none",
+                            border: `1px solid ${theme.palette.cardBorder}`,
+                        }}
+                    >
+                        <Certificate
+                            ref={certificateRef}
+                            recipientName="Sreang Lyhour"
+                            date="2024-04-27"
+                            badge={{
+                                name: "Advance Frontend",
+                                imageUrl: BadgeTest,
+                            }}
+                        />
+                    </Box>
                 </Box>
-            )}
-        </>
+
+                <Stack
+                    sx={{
+                        flexDirection: "row",
+                        gap: 2,
+                        justifyContent: "space-between",
+                        alignItems: "start",
+                        maxWidth: 1000,
+                        width: "100%",
+                        mx: "auto",
+                    }}
+                >
+                    <Button
+                        variant="outlined"
+                        color="success"
+                        sx={{
+                            maxWidth: 200,
+                            backgroundColor: theme.palette.customColors.green100,
+                            "&:hover": {
+                                backgroundColor: theme.palette.background.success,
+                            },
+                        }}
+                        onClick={handleCongrats}
+                    >
+                        <AutoAwesome />
+                    </Button>
+                    <Stack flexDirection={{ md: "row", xss: "column" }} gap={1}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleGenerateImage}
+                            sx={{ maxWidth: 200, color: theme.palette.customColors.white }}
+                            startIcon={<DownloadDoneOutlined />}
+                        >
+                            Get Certificate
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            sx={{ maxWidth: 200 }}
+                            onClick={handleCopyUrl}
+                            startIcon={<ContentCopyIcon />}
+                        >
+                            {copyButtonText}
+                        </Button>
+                    </Stack>
+                </Stack>
+                {isExploding && (
+                    <Box sx={{ position: "absolute", top: "20%", left: { md: "55%", xss: "50%" } }}>
+                        <ConfettiExplosion force={0.6} duration={3000} particleCount={150} width={1600} />
+                    </Box>
+                )}
+            </Stack>
+        </DashboardContainer>
     );
 };
 
