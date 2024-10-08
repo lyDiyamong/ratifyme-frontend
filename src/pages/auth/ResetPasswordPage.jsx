492727ee72bb8ecaaa562fa1b82a0ff2d1b03373
Sreypovprak <sreypovprak88@gmail.com>
2024-10-08 17:35:41 +0700

    refactor(51-enhanceSignupPage): refactor password field by using the existing component

--- src/pages/auth/ResetPasswordPage.jsx
+++ src/pages/auth/ResetPasswordPage.jsx
@@ -1,60 +1,35 @@
-import { useState, useEffect, useMemo } from "react";
+// React library import
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, Link, useParams } from "react-router-dom";
-import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Box, Typography, Button, LinearProgress, Stack } from "@mui/material";
+
+// MUI import
+import { Box, Typography, Button, Stack } from "@mui/material";
 import { VpnKeyOutlined } from "@mui/icons-material";
-import FormInput from "../../components/FormInput";
+
+// Custom import
+import PasswordFields from "../../components/auth/PasswordFields";
 import theme from "../../assets/themes";
 import RatifyMELogo from "../../assets/icons/RatfiyME.svg";
 import { SpinLoading } from "../../components/loading/SpinLoading";
-import OutletImageComponent from "./OutletImageTemplate";
 import { useResetPasswordMutation, useVerifyResetTokenQuery } from "../../store/api/auth/authApi";
-
-const schema = yup.object({
-    password: yup
-        .string()
-        .min(10, "⚠️ Your password must be at least 10 characters.")
-        .matches(/[a-z]/, "⚠️ Your password must contain at least one lowercase letter.")
-        .matches(/[A-Z]/, "⚠️ Your password must contain at least one uppercase letter.")
-        .matches(/\d/, "⚠️ Your password must contain at least one number.")
-        .matches(/[@$!%*?&]/, "⚠️ Your password must contain at least one special character.")
-        .required("Password is required"),
-    passwordConfirm: yup
-        .string()
-        .oneOf([yup.ref("password")], "⚠️ Passwords must match")
-        .required("Confirm password is required"),
-});
-
-const getPasswordStrength = (password) => {
-    let strength = 0;
-    if (password.length >= 10) strength += 20;
-    if (/[a-z]/.test(password)) strength += 20;
-    if (/[A-Z]/.test(password)) strength += 20;
-    if (/\d/.test(password)) strength += 20;
-    if (/[@$!%*?&]/.test(password)) strength += 20;
-    return strength;
-};
-
-const validatePassword = (password) => {
-    return {
-        hasMinLength: password.length >= 10,
-        hasLowercase: /[a-z]/.test(password),
-        hasUppercase: /[A-Z]/.test(password),
-        hasNumber: /\d/.test(password),
-        hasSpecialChar: /[@$!%*?&]/.test(password),
-    };
-};
+import OutletImageComponent from "./OutletImageTemplate";
+import { passwordSchema } from "../../utils/auth/passwordUtils";
 
 const ResetPasswordPage = () => {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
     const { token } = useParams();
     const [resetPassword] = useResetPasswordMutation();
-    
+
     const { data, error } = useVerifyResetTokenQuery(token, {
         skip: !token,
+    });
+
+    const schema = passwordSchema({
+        passwordName: "password",
+        passwordConfirmName: "passwordConfirm",
     });
 
     const {
@@ -71,10 +46,6 @@ const ResetPasswordPage = () => {
         resolver: yupResolver(schema),
     });
 
-    const watchPassword = watch("password", "");
-    const passwordStrength = useMemo(() => getPasswordStrength(watchPassword), [watchPassword]);
-    const validationState = useMemo(() => validatePassword(watchPassword), [watchPassword]);
-
     useEffect(() => {
         if (error) {
             navigate("/invalid-reset-password");
@@ -82,10 +53,19 @@ const ResetPasswordPage = () => {
     }, [error, navigate]);
 
     const onSubmit = async (data) => {
+        
         setLoading(true);
         try {
+            // Attempt to reset the password
             await resetPassword({ token, ...data }).unwrap();
+            // Navigate to success page if successful
             navigate("/reset-password-success");
+        } catch (err) {
+            console.error("Error resetting password:", err);
+            // Redirect to error page if there's an issue with the token
+            if (error) {
+                navigate("/invalid-reset-password");
+            }
         } finally {
             setLoading(false);
         }
@@ -106,7 +86,6 @@ const ResetPasswordPage = () => {
                     <Link to="/">
                         <Box component="img" src={RatifyMELogo} alt="RatifyME Logo" sx={{ width: 150, height: 150 }} />
                     </Link>
-
                     <Box
                         component="div"
                         width={70}
@@ -121,7 +100,6 @@ const ResetPasswordPage = () => {
                     >
                         <VpnKeyOutlined sx={{ fontSize: "32px", color: theme.palette.primary.dark }} />
                     </Box>
-
                     <Box my={3}>
                         <Typography variant="h3" fontWeight={theme.fontWeight.semiBold} mb={1}>
                             Set New Password
@@ -130,128 +108,15 @@ const ResetPasswordPage = () => {
                             Please enter a new password below.
                         </Typography>
                     </Box>
-
-                    <Stack spacing={2}>
-                        <FormInput
-                            name="password"
-                            label="New password"
-                            control={control}
-                            required
-                            type="password"
-                            errorMessage={errors.password?.message}
-                            aria-label="Password"
-                        />
-
-                        <Box sx={{ display: "flex", gap: 0.5 }}>
-                            {[1, 2, 3, 4, 5].map((_, index) => (
-                                <LinearProgress
-                                    key={index}
-                                    variant="determinate"
-                                    value={passwordStrength >= (index + 1) * 20 ? 100 : 0}
-                                    sx={{
-                                        flex: 1,
-                                        height: 5,
-                                        borderRadius: 5,
-                                        backgroundColor: theme.palette.customColors.lightGray,
-                                        "& .MuiLinearProgress-bar": {
-                                            backgroundColor:
-                                                passwordStrength < 40
-                                                    ? "red"
-                                                    : passwordStrength < 80
-                                                    ? "orange"
-                                                    : "green",
-                                        },
-                                    }}
-                                />
-                            ))}
-                        </Box>
-                        <Stack direction="row">
-                            <Typography
-                                variant="caption"
-                                sx={{
-                                    color:
-                                        passwordStrength === 0
-                                            ? "text.secondary"
-                                            : passwordStrength < 40
-                                            ? "error.main"
-                                            : passwordStrength < 80
-                                            ? "warning.main"
-                                            : "success.main",
-                                }}
-                            >
-                                Password Strength:{" "}
-                            </Typography>
-                            <Typography
-                                variant="caption"
-                                sx={{
-                                    fontWeight: "bold",
-                                    color:
-                                        passwordStrength === 0
-                                            ? "text.secondary"
-                                            : passwordStrength < 40
-                                            ? "error.main"
-                                            : passwordStrength < 80
-                                            ? "warning.main"
-                                            : "success.main",
-                                }}
-                            >
-                                {passwordStrength === 0
-                                    ? "Empty"
-                                    : passwordStrength < 40
-                                    ? "Weak"
-                                    : passwordStrength < 80
-                                    ? "Medium"
-                                    : "Strong"}
-                            </Typography>
-                        </Stack>
-
-                        <Box mt={2}>
-                            <Typography variant="body2" sx={{ color: "text.secondary", fontWeight: "bold" }}>
-                                Your password must contain:
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color={validationState.hasMinLength ? "success.main" : "text.secondary"}
-                            >
-                                {validationState.hasMinLength ? "✔" : "✖"} At least 10 characters
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color={validationState.hasLowercase ? "success.main" : "text.secondary"}
-                            >
-                                {validationState.hasLowercase ? "✔" : "✖"} Contains a lowercase letter
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color={validationState.hasUppercase ? "success.main" : "text.secondary"}
-                            >
-                                {validationState.hasUppercase ? "✔" : "✖"} Contains an uppercase letter
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color={validationState.hasNumber ? "success.main" : "text.secondary"}
-                            >
-                                {validationState.hasNumber ? "✔" : "✖"} Contains a number
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color={validationState.hasSpecialChar ? "success.main" : "text.secondary"}
-                            >
-                                {validationState.hasSpecialChar ? "✔" : "✖"} Contains a special character
-                            </Typography>
-                        </Box>
-
-                        <FormInput
-                            name="passwordConfirm"
-                            label="Confirm new password"
-                            control={control}
-                            required
-                            type="password"
-                            errorMessage={errors.passwordConfirm?.message}
-                            aria-label="Confirm Password"
-                        />
-                    </Stack>
-
+                    {/* Use the new PasswordFields component */}
+                    <PasswordFields
+                        control={control}
+                        errors={errors}
+                        watch={watch}
+                        schema={schema}
+                        passwordName="password"
+                        passwordConfirmName="passwordConfirm"
+                    />{" "}
                     <Button
                         fullWidth
                         variant="contained"
