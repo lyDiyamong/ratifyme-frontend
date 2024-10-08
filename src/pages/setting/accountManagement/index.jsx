38ec12417094adb2cf86e3db5594ab930e4f5db9
Khun Malen <malen8622@gmail.com>
2024-10-08 14:34:20 +0700

    refactor(53-refactorUserInfo): change some code in index.js accountManagement

--- src/pages/setting/accountManagement/index.jsx
+++ src/pages/setting/accountManagement/index.jsx
@@ -2,14 +2,13 @@
 import { useState } from "react";
 
 // MUI import
-import { Tabs, Tab, Stack, Button, Typography } from "@mui/material";
+import { Tabs, Tab } from "@mui/material";
 
 // Custom import
 import PageTitle from "../../../components/PageTitle";
 import DashboardContainer from "../../../components/styles/DashboardContainer";
-import UserProfile from "./UserProfile";
-import theme from "../../../assets/themes";
-import ProfileSetting from "./ProfileSetting";
+import UserProfile from "./UserProfiles/index";
+import ProfileSetting from "./UserProfiles/ProfileSetting";
 
 const AccountManagement = () => {
     const [value, setValue] = useState(0);
@@ -29,32 +28,8 @@ const AccountManagement = () => {
             </Tabs>
 
             {/* Conditional rendering based on the selected tab */}
-            {value === 0 && (
-                <Stack spacing={2}>
-                    <UserProfile />
-                </Stack>
-            )}
-            {value === 1 && (
-                <Stack spacing={2} sx={{ p: 2 }}>
-                    <Typography variant="h4" sx={{ fontWeight: theme.fontWeight.semiBold }}>
-                        Profile Settings
-                    </Typography>
-                    <Button
-                        sx={{
-                            color: theme.palette.primary.main,
-                            fontWeight: theme.fontWeight.semiBold,
-                            backgroundColor: theme.palette.action.hover,
-                            maxWidth: 200,
-                            height: 40,
-                            textTransform: "none",
-                            borderRadius: theme.customShape.btn,
-                        }}
-                    >
-                        Go to Settings
-                    </Button>
-                    <ProfileSetting />
-                </Stack>
-            )}
+            {value === 0 && <UserProfile />}
+            {value === 1 && <ProfileSetting />}
         </DashboardContainer>
     );
 };
