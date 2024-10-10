48862a091b68a458fb74ffbd912b8da77183795c
Sreypovprak <sreypovprak88@gmail.com>
2024-10-10 19:17:22 +0700

    add(51-enhanceSignupPage): add page title above the signup form

--- src/components/auth/GeneralInfoFields.jsx
+++ src/components/auth/GeneralInfoFields.jsx
@@ -2,6 +2,8 @@ import DateSelectionForm from "../DateSelectionForm";
 import SelectForm from "../SelectionForm";
 import FormInput from "../FormInput";
 import { Stack } from "@mui/system";
+import { Typography } from "@mui/material";
+import theme from "../../assets/themes";
 
 const GeneralInfoFields = ({ control, schema }) => {
     const genderOptions = [
@@ -11,6 +13,13 @@ const GeneralInfoFields = ({ control, schema }) => {
 
     return (
         <Stack gap={2}>
+            <Typography variant="body1" fontWeight={theme.fontWeight.semiBold} fontSize="24px">
+                General Information
+            </Typography>
+            <Typography sx={{ fontSize: theme.typography.body1, color: theme.palette.customColors.gray500, mb: 1 }}>
+                Provide basic personal information such as name, gender, and date of birth details to set up your
+                profile.
+            </Typography>
             <FormInput
                 name="firstName"
                 label="First Name"
--- src/components/auth/InstitutionInfoFields.jsx
+++ src/components/auth/InstitutionInfoFields.jsx
@@ -1,10 +1,19 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import FormInput from "../FormInput";
 import PhoneNumberForm from "../PhoneNumberForm";
+import theme from "../../assets/themes";
 
 const InstitutionInfoFields = ({ control, schema }) => {
     return (
         <Stack gap={2}>
+            <Typography variant="body1" fontWeight={theme.fontWeight.semiBold} fontSize="24px" mb={1}>
+                Institution Information
+            </Typography>
+            <Typography sx={{ fontSize: theme.typography.body1, color: theme.palette.customColors.gray500, mb: 1 }}>
+                Share information about your affiliated institution, including its name, email, phone number and website, for institutional
+                verification.
+            </Typography>
+
             <FormInput name="institutionName" label="Institution Name" control={control} required />
 
             <FormInput
@@ -15,7 +24,12 @@ const InstitutionInfoFields = ({ control, schema }) => {
                 schema={schema?.fields.institutionEmail}
             />
 
-            <PhoneNumberForm name="institutionPhoneNumber" label="Institution Phone Number" control={control} required />
+            <PhoneNumberForm
+                name="institutionPhoneNumber"
+                label="Institution Phone Number"
+                control={control}
+                required
+            />
 
             <FormInput name="institutionWebsiteUrl" label="Website URL" control={control} schema={schema?.fields.url} />
         </Stack>
