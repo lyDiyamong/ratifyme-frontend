608337b751443cbba4bf1a83e3f7ef7f400da1d6
RothaSAMON <samonrotha@gmail.com>
2024-10-14 14:13:35 +0700

    change(59-OrgInfoTab): modify the landing title

--- src/pages/landing/HeroSection.jsx
+++ src/pages/landing/HeroSection.jsx
@@ -73,7 +73,7 @@ const HeroSection = () => {
                                 my: "0",
                             }}
                         >
-                            Unlock the future of credentials with accessible, Techa Cambodia Kamjea
+                            Unlock the future of credentials with accessible, flexible, and verifiable
                         </Box>
                         <Box
                             component="p"
