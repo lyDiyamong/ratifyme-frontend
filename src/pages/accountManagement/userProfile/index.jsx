b02c7817a09218161d619dab7ba055f797e639dd
RothaSAMON <samonrotha@gmail.com>
2024-10-15 13:56:11 +0700

    change(59-OrgInfoTab): change style of the Bio content and base on roles

--- src/pages/accountManagement/userProfile/index.jsx
+++ src/pages/accountManagement/userProfile/index.jsx
@@ -10,20 +10,18 @@ const UserProfile = () => {
     return (
         <Stack>
             <Stack gap={3}>
-                <Grid container spacing={3} sx={{ height: "100%" }}>
+                <Grid container spacing={3}>
                     {/* Profile Header and Bio Content in the same parent */}
-                    <Grid item xss={12} md={4} sx={{ display: "flex", flexDirection: "column" }}>
-                        <Stack spacing={2} direction="column">
+                    <Grid item xss={12} md={4}>
+                        <Stack spacing={3} direction="column">
                             <ProfileHeader />
                             <BioContent />
                         </Stack>
                     </Grid>
 
                     {/* Profile Info on the right side */}
-                    <Grid item xss={12} md={8} sx={{ height: "100%" }}>
-                        <Stack>
-                            <ProfileInfoContainer />
-                        </Stack>
+                    <Grid item xss={12} md={8}>
+                        <ProfileInfoContainer />
                     </Grid>
                 </Grid>
             </Stack>
