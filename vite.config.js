fe52e1657c8945d1ce3f3307051a84f26d614b87
BunthongRorn <bunthongran579@gmail.com>
2024-10-16 17:21:42 +0700

    add XML sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { copyFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +15,13 @@ export default defineConfig({
       'chunk-QMM7ELEV.js',
       'chunk-YL7LKWCF.js'
     ]
+  },
+  build: {
+    outDir: 'dist', // Output directory for the build
+    emptyOutDir: true, // Clears the output directory before building
+  },
+  // Hook to copy the sitemap.xml after building the project
+  buildEnd: () => {
+    copyFileSync(resolve(__dirname, 'sitemap.xml'), resolve(__dirname, 'dist/sitemap.xml'));
   }
-})
+});
