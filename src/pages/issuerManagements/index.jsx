d882941088556a5ffdbe776db0fb39b2dcac0026
Khun Malen <malen8622@gmail.com>
2024-10-17 11:55:05 +0700

    refactor(56-tableFeature): make index.jsx in issuer management more clean

--- src/pages/issuerManagements/index.jsx
+++ src/pages/issuerManagements/index.jsx
@@ -1,26 +1,20 @@
-//React Libray
-import { useState } from "react";
-
 // Custom import
 import OrganizationCard from "../../components/OrganizationCard";
 import AnBSchoolLogo from "../../assets/images/AnBSchoolLogo.svg";
 import DashboardContainer from "../../components/styles/DashboardContainer";
 import InviteIssuerPage from "./InviteIssuerPage";
 import TableIssuer from "./TableIssuer";
-import SearchBar from "../../components/searchBars/SearchBar";
 import PageTitle from "../../components/PageTitle";
 
 const IssuerManagement = () => {
-    const [searchQuery, setSearchQuery] = useState("");
     return (
         <DashboardContainer>
-            {/* <SearchBar onSearch={setSearchQuery} /> */}
             {/* <InviteIssuerPage /> */}
             <PageTitle
                 title="Issuer Managements"
                 subtitle="Manage issuer accounts, invite new issuers, and track their statuses all in one place."
             />
-            <TableIssuer searchQuery={searchQuery} />
+            <TableIssuer/>
         </DashboardContainer>
     );
 };
