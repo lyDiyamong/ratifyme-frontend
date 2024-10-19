6c7d2e256f67d6c41995997fd6f34ef231f2f4c6
Sreypovprak <sreypovprak88@gmail.com>
2024-10-19 20:42:04 +0700

    add(51-enhanceSignupPage): add tab for tables view

--- src/pages/issuerManagements/index.jsx
+++ src/pages/issuerManagements/index.jsx
@@ -1,16 +1,28 @@
-// Custom import
+// Custom Imports
 import DashboardContainer from "../../components/styles/DashboardContainer";
 import TableIssuer from "./TableIssuer";
+import TableIssuerInvitation from "./TableIssuerInvitation";
 import PageTitle from "../../components/PageTitle";
+import CustomTabs from "../../components/tabs/customTabs";
 
+// Issuer Management Component
 const IssuerManagement = () => {
+
+    const tabs = ["Issuer List", "Invited Issuers"];
+    const tabContent = [TableIssuer, TableIssuerInvitation];
+
     return (
         <DashboardContainer>
             <PageTitle
-                title="Issuer Managements"
+                title="Issuer Management"
                 subtitle="Manage issuer accounts, invite new issuers, and track their statuses all in one place."
             />
-            <TableIssuer/>
+
+            <CustomTabs
+                tabs={tabs}
+                tabContent={tabContent}
+                searchQuery=""
+            />
         </DashboardContainer>
     );
 };
