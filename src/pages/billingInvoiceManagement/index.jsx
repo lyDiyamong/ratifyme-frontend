2997dffafaf7194ae52281e8f18e2ba6be660f8f
Khun Malen <malen8622@gmail.com>
2024-10-19 12:19:08 +0700

    add(56-tableFeature): add api feature to table billing and invoice

--- src/pages/billingInvoiceManagement/index.jsx
+++ src/pages/billingInvoiceManagement/index.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 // Mui import
 
 // Custom import
-import TableCustom from "../../components/TableCustomFront";
+import TableCustom from "../../components/TableCustom";
 import MenuSelection from "../../components/TableAction/MenuSelection";
 import FormatDate from "../../utils/formatDate";
 import DashboardContainer from "../../components/styles/DashboardContainer";
@@ -12,16 +12,36 @@ import PageTitle from "../../components/PageTitle";
 import SkeletonLoading from "../../components/loading/SkeletonLoading";
 import AlertMessage from "../../components/alert/AlertMessage";
 import useCatchStatus from "../../hooks/useCatchStatus";
+import NoRecordData from "../../components/NoRecordData";
 
 // Api import
 import { useGetSubscritptionQuery } from "../../store/api/subscription/subscriptionApi";
+import { useState } from "react";
 
 const BillingInvoiceManagement = () => {
     // Navigate hook
     const navigate = useNavigate();
 
+    // Pagination & Sorting & Limiting & Searching
+    const [currentPage, setCurrentPage] = useState(false);
+    const [rowsPerPage, setRowsPerPage] = useState(1);
+    const [sortColumn, setSortColumn] = useState("name");
+    const [sortOrder, setSortOrder] = useState("-name");
+    const [searchQuery, setSearchQuery] = useState("");
+
     // Api fetching hook
-    const { data: response, isLoading, isError, error } = useGetSubscritptionQuery();
+    const {
+        data: response,
+        isLoading,
+        isError,
+        error,
+    } = useGetSubscritptionQuery({
+        page: currentPage,
+        limit: rowsPerPage,
+        sort: sortColumn,
+        order: sortOrder,
+        search: searchQuery,
+    });
     const subscriptions = response?.data;
 
     // Error custom hook
@@ -60,6 +80,29 @@ const BillingInvoiceManagement = () => {
         },
     ];
 
+    // Handle page change
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
+
+    // Handle rows per page change
+    const handleRowsPerPageChange = (newLimit) => {
+        setRowsPerPage(newLimit);
+    };
+
+    // Handle sorting change
+    const handleSortChange = (column) => {
+        const newSortOrder = sortOrder === "-name" ? "name" : "-name";
+        setSortColumn(column);
+        setSortOrder(newSortOrder);
+    };
+
+    // Handle searching
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+        setCurrentPage(1);
+    };
+
     return (
         // ============ Start BillingInvoiceManagement ============
         <DashboardContainer>
@@ -74,7 +117,23 @@ const BillingInvoiceManagement = () => {
                 <SkeletonLoading num={5} />
             ) : (
                 // Billing and Invoice Table
-                <TableCustom title="Billing and Invoice" data={subscriptions} columns={columns} />
+                <TableCustom
+                    title="Billing and Invoice"
+                    data={subscriptions}
+                    columns={columns}
+                    pagination
+                    totalRows={response?.total || 0}
+                    currentPage={currentPage}
+                    rowsPerPage={rowsPerPage}
+                    onPageChange={handlePageChange}
+                    onRowsPerPageChange={handleRowsPerPageChange} 
+                    onSortChange={handleSortChange} 
+                    sortColumn={sortColumn}
+                    sortOrder={sortOrder}
+                    onSearch={handleSearch}
+                >
+                    {subscriptions?.length === 0 && <NoRecordData />}
+                </TableCustom>
             )}
         </DashboardContainer>
         // ============ End BillingInvoiceManagement ============
