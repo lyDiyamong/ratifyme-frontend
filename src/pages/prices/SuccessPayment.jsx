6a238e896df40f577fb8968e85959afe07531435
RothaSAMON <samonrotha@gmail.com>
2024-10-20 12:21:53 +0700

    chnage(59-OrgInfotab): clear comments

--- src/pages/prices/SuccessPayment.jsx
+++ src/pages/prices/SuccessPayment.jsx
@@ -1,7 +1,6 @@
-import { Box, Typography, Button, Paper, Divider } from "@mui/material";
+import { Box, Typography, Button, Paper, Divider, } from "@mui/material";
 import theme from "../../assets/themes";
 import { CheckCircleRounded, ChevronLeft } from "@mui/icons-material";
-import { Stack } from "@mui/system";
 
 const SuccessPayment = () => {
     return (
@@ -15,7 +14,7 @@ const SuccessPayment = () => {
                     borderRadius: "20px",
                     boxShadow: theme.customShadows.default,
                     m: 3,
-                    backgroundColor: 'white'
+                    backgroundColor: "white",
                 }}
             >
                 {/* Success Icon */}
@@ -42,40 +41,6 @@ const SuccessPayment = () => {
                 <Typography color="textSecondary" mb={2}>
                     Your payment was processed successfully. Thank you for your purchase!
                 </Typography>
-
-                {/* <Divider sx={{ borderStyle: "dashed", my: 3 }} />
-
-                <Stack textAlign="left" justifyContent="center">
-                    <Typography color="textSecondary" mb={1} textAlign="center">
-                        Total Amount
-                    </Typography>
-                    <Typography variant="h2" textAlign="center" fontWeight={theme.fontWeight.bold} gutterBottom>
-                        $ 99.99
-                    </Typography>
-
-                    <Stack justifyContent="start" alignItems="start">
-                        <Typography variant="body2" fontWeight="bold">
-                            Order Number:
-                        </Typography>
-                        <Typography variant="body2" mb={1}>
-                            #12345
-                        </Typography>
-
-                        <Typography variant="body2" fontWeight="bold">
-                            Payment Date:
-                        </Typography>
-                        <Typography variant="body2"  mb={1}>May 28, 2024</Typography>
-
-                        <Typography variant="body2" fontWeight="bold">
-                            Sender Name:
-                        </Typography>
-                        <Typography variant="body2">Sreang Lyhour</Typography>
-                    </Stack>
-                </Stack>
-
-                <Divider sx={{ borderStyle: "dashed", my: 3 }} /> */}
-
-                {/* =================================== */}
 
                 <Divider sx={{ borderStyle: "dashed", my: 3 }} />
 
@@ -134,8 +99,6 @@ const SuccessPayment = () => {
                 </Box>
 
                 <Divider sx={{ borderStyle: "dashed", my: 3 }} />
-
-                {/* =================================== */}
 
                 {/* View Order Details button */}
                 <Button
