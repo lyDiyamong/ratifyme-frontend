95283aed3609879abca047aa7ff4cc4e64f1178f
Lyhour12 <slyhour69@gmail.com>
2024-10-21 15:47:01 +0700

    fix(60-refactorSendBadge): give condition to acess button by role

--- src/pages/badgeMangements/index.jsx
+++ src/pages/badgeMangements/index.jsx
@@ -1,13 +1,16 @@
+import { useSelector } from "react-redux";
 import PageTitle from "../../components/PageTitle";
 import SearchBar from "../../components/SearchBar";
 import DashboardContainer from "../../components/styles/DashboardContainer";
 import BadgeList from "./BadgeList";
 
 const BadgeManagement = () => {
+    const { roleId } = useSelector((state) => state.global);
+    const allowRole = roleId === 3 ? true : false;
     return (
         <DashboardContainer sx={{ pb: 4 }}>
             <PageTitle title="Badge Management " />
-            <SearchBar showButton={true} textInButton="Add Badge">
+            <SearchBar showButton={allowRole} textInButton="Add Badge">
                 <BadgeList />
             </SearchBar>
         </DashboardContainer>
