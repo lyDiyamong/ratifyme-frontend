86df449b2488defa6052bb7a67f37ba1132a46a5
Khun Malen <malen8622@gmail.com>
2024-10-23 10:49:28 +0700

    fix(71-refactorAllTable): remove search bar from the report index

--- src/pages/report/index.jsx
+++ src/pages/report/index.jsx
@@ -1,22 +1,17 @@
-// React Library
-import { useState } from "react";
-
 // MUI Import
 import DashboardContainer from "../../components/styles/DashboardContainer";
 
 // Custom Import
 import TableReport from "./TableReport";
-import SearchBar from "../../components/searchBars/SearchBar";
 import TableChart from "./TableChart";
 
 // ============ Start Report Page ============
 const reportManagement = () => {
-    const [searchQuery, setSearchQuery] = useState("");
     return (
         <DashboardContainer>
-            <SearchBar onSearch={setSearchQuery} />
+           
             <TableChart />
-            <TableReport searchQuery={searchQuery} />
+            <TableReport />
         </DashboardContainer>
     );
 };
