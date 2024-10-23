a8a1466f64f25b9e5f0794ba719b6be2de72161b
lyDiyamong <diyamong26@gmail.com>
2024-10-23 14:35:32 +0700

    fix(69-credIntegrate): add comment to landing folders to match the convention guide

--- src/pages/landing/OurCustomer.jsx
+++ src/pages/landing/OurCustomer.jsx
@@ -1,51 +1,49 @@
-//Import React 
-import React from "react";
-//Import Mui
 
+// MUI import
 import { useTheme } from "@mui/material/styles";
-import { Box} from  "@mui/material";
+import { Box } from "@mui/material";
+
+// Custom import
 import LogoCards from "./CompanyLogoCard.jsx";
 
-
-
-
-
 //Our Customer Component
-function OurCustomer(){
-
+function OurCustomer() {
     const theme = useTheme();
     return (
         //============ Start Our Customer Section  ============
-        <Box component="section"
+        <Box
+            component="section"
             sx={{
-            display: "flex",
-            flexDirection:"column",
-            justifyContent: "end",
-            margin: "0",
-            width: "100%",
-            maxWidth: "none",
-            backgroundColor: theme.palette.primary.dark,
-            minHeight: "248px",
-            padding: "0",
-        }}
-        >{/* Start Tittle   */}
-            <Box component= "div"
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "end",
+                margin: "0",
+                width: "100%",
+                maxWidth: "none",
+                backgroundColor: theme.palette.primary.dark,
+                minHeight: "248px",
+                padding: "0",
+            }}
+        >
+            {/* Start Tittle   */}
+            <Box
+                component="div"
                 sx={{
                     color: "white",
                     textAlign: "center",
-                    
+
                     fontWeight: theme.fontWeight.bold,
-                    fontSize: theme.typography.h4
-                }}>
+                    fontSize: theme.typography.h4,
+                }}
+            >
                 <Box component="p">Our Customer</Box>
             </Box>
-        {/*  End Tittle   */}
-            
+            {/*  End Tittle   */}
+
             <LogoCards />
-        </Box> 
-         //============ End Our Customer Section  ============     
+        </Box>
+        //============ End Our Customer Section  ============
     );
-};
+}
 
 export default OurCustomer;
-
