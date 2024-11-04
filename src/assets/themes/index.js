89d589ae4b2a54c39630e60cb16c352ec13bc2ee
Khun Malen <malen8622@gmail.com>
2024-11-04 15:05:46 +0700

    fix(93-fixError): prepare code in all main files

--- src/assets/themes/index.js
+++ src/assets/themes/index.js
@@ -1,3 +1,4 @@
+// MUI import
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+// React library import
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
 import store from "./store/index.js";
 
 createRoot(document.getElementById("root")).render(
